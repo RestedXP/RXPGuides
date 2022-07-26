5912a081fd5dd59ad715362c6c0d53167cd9001e
Zarant <26726709+Zarant@users.noreply.github.com>
2022-07-26 13:41:05 -0300

    fixed db error on new characters

--- GuideWindow.lua
+++ GuideWindow.lua
@@ -1577,7 +1577,7 @@ function RXPFrame.GenerateMenuTable()
         end
     })
 
-    if addon.settings.db.profile.enableTrackerReport then
+    if addon.settings.db and addon.settings.db.profile.enableTrackerReport then
         table.insert(menuList, {
             text = "Leveling report",
             notCheckable = 1,
