3c8fde027549b3d20fcd6033674bfd29a4db8170
SabreValkyrn <sabre@valkyrnstudios.net>
2022-08-29 18:37:53 -0700

    Revert "Fix new player initialization"
    This reverts commit d56caa407b4e2c9d5860b7a20c5b6bd9107f8eee.

--- Communications.lua
+++ Communications.lua
@@ -155,7 +155,7 @@ function addon.comms:TallyGroup(xp)
         if not name then break end
 
         if not self.players[name] then
-            self.players[name] = {
+            self.players[name].timePlayed = {
                 xp = xp,
                 timePlayed = 0,
                 class = UnitClassBase("party" .. i)
