b0462ae85bf32d663e7d4675203f2d2eb8438729
Calamityrxp <105675676+Calamityrxp@users.noreply.github.com>
2022-09-06 12:25:01 -0500

    Firebolt Rank 2 removed

--- Guides/RestedXP Horde 1-13 Troll-Orc.lua
+++ Guides/RestedXP Horde 1-13 Troll-Orc.lua
@@ -845,10 +845,6 @@ step << Warlock
     .goto Durotar,54.4,41.2
     .train 980 >>Train Curse of Agony
     .train 5782 >>Train Fear
-step << Warlock
-    #xprate <1.5
-.goto Durotar,54.7,41.5
-    .vendor >>Buy the Firebolt r2 book and use it
 step << Warrior/Rogue
     #xprate <1.5
     .goto Durotar,54.2,41.9
