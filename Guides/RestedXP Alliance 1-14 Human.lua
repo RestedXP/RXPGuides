e1437379ba31051693ab064de9a9c94b12e34c03
yamanlol <109969610+yamanlol@users.noreply.github.com>
2022-09-17 11:19:19 +0200

    2h sword warrior training tbc only

--- Guides/RestedXP Alliance 1-14 Human.lua
+++ Guides/RestedXP Alliance 1-14 Human.lua
@@ -1030,7 +1030,7 @@ step
     >>Talk to Morgan Pestle in the building. Use the rockets for AoE damage or to splitpull packs
     .link https://www.youtube.com/watch?v=H-IwZ6P-ldY >> Click here for a guide on splitpulling (long but informative)
     .turnin 61,1 >> Turn in Shipment to Stormwind
-step << Warrior
+step << Warrior tbc
     .goto StormwindClassic,57.1,57.7
     .trainer >> Train 2h Swords
 step << Priest
