57b5fd939a41885b2fa6222152d9099363c59119
SabreValkyrn <sabre@valkyrnstudios.net>
2022-09-18 12:18:15 -0700

    Integrate help, add buttons to top

--- Help.lua
+++ Help.lua
@@ -2,35 +2,22 @@ local _, addon = ...
 
 local H = {}
 
-H['Why do I get an error when importing my guides?'] = [[
-It depends on the error, the 3 most common ones are listed below:
-
-|cffffcc00undefined|r
-  If your string file says undefined, your battle.net is not set on our site. Try clearing your browser cache and trying again.
-
-|cffffcc00Invalid Value|r
-  This means the guide didn't connect to your battle.net, please appear online or restart your game
-
-|cffffcc00Invalid Import String|r or |cffffcc00Unable to parse guide|r
-  This likely means your battle.net does not match the one on your account, please see Discord's #open-ticket to get your battle.net adjusted.
-]]
-
 H["I'm missing a lot of exp, why?"] = [[
 You should be grinding mobs between quests, not just moving from objective to objective.
 
 This guide is based on speed - which means less quests to turn in. Grinding from quest to quest makes up for the lost xp.
 ]]
 
-H["Why does the guide skip level brackets?"] = [[
+H["Why is my guide missing levels?"] = [[
 The level ranges on our guides are intended for standard exp rates, do not worry about following the leveling brackets while on 50% exp.
 
 Just follow the guide the best you can.
 ]]
 
-H["Why am I missing guide chapters?"] = [[
+H["Why is my guide skipping lots of steps or zones?"] = [[
 These are inefficient areas that we outright removed, don't worry, we added steps to the start of the next guide to get you caught up.
 
-You can view them by turning your slider down to 1.0x from 1.5x in your options.
+You can view the full guide without any skips by turning your slider to 1x from 1.5x in your options
 ]]
 
 addon.help = H
--- SettingsPanel.lua
+++ SettingsPanel.lua
@@ -100,7 +100,6 @@ function addon.settings:InitializeSettings()
 
     self:CreateAceOptionsPanel()
     self.CreateImportOptionsPanel()
-    self.CreateHelpOptionsPanel()
     self:MigrateSettings()
     self:UpdateMinimapButton()
 
@@ -521,17 +520,28 @@ function addon.settings:CreateAceOptionsPanel()
 
     local optionsTable = {
         type = "group",
-        name = addon.title,
+        name = fmt("%s - %s", addon.title, addon.versionText),
         get = GetProfileOption,
         set = SetProfileOption,
         childGroups = "tab",
         args = {
-            buffer = { -- Buffer hacked in right-aligned icon
-                order = 1,
-                name = addon.versionText,
-                type = "description",
-                width = "full",
-                fontSize = "medium"
+            discordButton = {
+                order = 1.0,
+                name = L("Join Discord"), -- TODO locale
+                type = "execute",
+                width = "normal",
+                func = function()
+                    addon.url = "https://discord.gg/restedxp"
+                    _G.StaticPopup_Show("RXP_Link")
+                    addon.url = nil
+                end
+            },
+            feedbackButton = {
+                order = 1.1,
+                name = L("Open Feedback Form"),
+                type = "execute",
+                width = "normal",
+                func = addon.comms.OpenBugReport
             },
             generalSettings = {
                 type = "group",
@@ -1120,10 +1130,16 @@ function addon.settings:CreateAceOptionsPanel()
                     }
                 }
             },
+            helpPanel = {
+                type = "group",
+                name = _G.HELP_LABEL,
+                order = 9,
+                args = {}
+            },
             advancedSettings = {
                 type = "group",
                 name = L("Advanced Settings"),
-                order = 5,
+                order = 10,
                 args = {
                     enableBetaFeatures = {
                         name = L("Enable Beta Features"),
@@ -1237,6 +1253,26 @@ function addon.settings:CreateAceOptionsPanel()
             }
         }
     }
+
+    -- Build FAQ items
+    local faqBatch = 2
+    for q, a in pairs(addon.help) do
+        optionsTable.args.helpPanel.args[faqBatch .. "q"] = {
+            order = faqBatch + 0.1,
+            name = q,
+            type = "header",
+            width = "full"
+        }
+
+        optionsTable.args.helpPanel.args[faqBatch .. "a"] = {
+            order = faqBatch + 0.2,
+            name = a,
+            type = "description",
+            width = "full",
+            fontSize = "medium"
+        }
+        faqBatch = faqBatch + 1
+    end
 
     AceConfig:RegisterOptionsTable(addon.title, optionsTable)
 
@@ -1381,85 +1417,4 @@ function addon.settings:RefreshProfile()
     addon.updateMap = true
     addon.RXPFrame.GenerateMenuTable()
     addon.RXPFrame.SetStepFrameAnchor()
-end
-
-function addon.settings.CreateHelpOptionsPanel()
-    local importOptionsTable = {
-        type = "group",
-        name = fmt("%s %s", addon.title, _G.HELP_LABEL),
-        handler = addon.settings,
-        args = {
-            feedbackButton = {
-                order = 0.1,
-                name = L("Open Feedback Form"),
-                type = "execute",
-                width = "normal",
-                func = addon.comms.OpenBugReport
-            },
-            discordButton = {
-                order = 0.2,
-                name = _G.HELP_FRAME_TITLE,
-                type = "execute",
-                width = "normal",
-                func = function()
-                    addon.url = "https://discord.gg/restedxp"
-                    _G.StaticPopup_Show("RXP_Link")
-                    addon.url = nil
-                end
-            },
-            faqHeader = {
-                order = 1,
-                name = " ",
-                type = "description",
-                width = "full",
-                fontSize = "medium"
-
-            }
-        }
-    }
-
-    local batch = 2
-    for q, a in pairs(addon.help) do
-        importOptionsTable.args[batch .. "q"] = {
-            order = batch + 0.1,
-            name = q,
-            type = "header",
-            width = "full"
-        }
-
-        importOptionsTable.args[batch .. "a"] = {
-            order = batch + 0.2,
-            name = a,
-            type = "description",
-            width = "full",
-            fontSize = "medium"
-        }
-        batch = batch + 1
-    end
-
-    AceConfig:RegisterOptionsTable(addon.title .. "/Help", importOptionsTable)
-
-    addon.settings.gui.help = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(
-                                  addon.title .. "/Help", _G.HELP_LABEL,
-                                  addon.title)
-
-    -- Ace3 ConfigDialog doesn't support embedding icons in header
-    -- Directly references Ace3 built frame object
-    -- Hackery ahead
-
-    importFrame = addon.settings.gui.help.obj.frame
-    importFrame.icon = importFrame:CreateTexture()
-    importFrame.icon:SetTexture("Interface\\AddOns\\" .. addonName ..
-                                    "\\Textures\\rxp_logo-64")
-    importFrame.icon:SetPoint("TOPRIGHT", -5, -5)
-
-    importFrame.text = importFrame:CreateFontString(nil, "OVERLAY")
-    importFrame.text:ClearAllPoints()
-    importFrame.text:SetPoint("CENTER", importFrame, 1, 1)
-    importFrame.text:SetJustifyH("LEFT")
-    importFrame.text:SetJustifyV("CENTER")
-    importFrame.text:SetTextColor(1, 1, 1)
-    importFrame.text:SetFont(addon.font, 14)
-    importFrame.text:SetText("")
-
 end
