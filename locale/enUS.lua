d74d2354e72c083dd77307d26ac4566415a3f39b
SabreValkyrn <sabre@valkyrnstudios.net>
2022-10-28 13:31:47 -0700

    Update target bindings

--- locale/enUS.lua
+++ locale/enUS.lua
@@ -12,14 +12,23 @@ _G["BINDING_NAME_" .. "CLICK RXPItemFrameButton3:LeftButton"] =
 _G["BINDING_NAME_" .. "CLICK RXPItemFrameButton4:LeftButton"] =
     "Active Item Button 4"
 
-_G["BINDING_NAME_" .. "CLICK RXPTargetFrameButton1:LeftButton"] =
-    "Active Target Button 1"
-_G["BINDING_NAME_" .. "CLICK RXPTargetFrameButton2:LeftButton"] =
-    "Active Target Button 2"
-_G["BINDING_NAME_" .. "CLICK RXPTargetFrameButton3:LeftButton"] =
-    "Active Target Button 3"
-_G["BINDING_NAME_" .. "CLICK RXPTargetFrameButton4:LeftButton"] =
-    "Active Target Button 4"
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_FriendlyButton1:LeftButton"] =
+    "Active Friendly Target Button 1"
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_FriendlyButton2:LeftButton"] =
+    "Active Friendly Target Button 2"
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_FriendlyButton3:LeftButton"] =
+    "Active Friendly Target Button 3"
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_FriendlyButton4:LeftButton"] =
+    "Active Friendly Target Button 4"
+
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_EnemyButton1:LeftButton"] =
+    "Active Enemy Target Button 1"
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_EnemyButton2:LeftButton"] =
+    "Active Enemy Target Button 2"
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_EnemyButton3:LeftButton"] =
+    "Active Enemy Target Button 3"
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_EnemyButton4:LeftButton"] =
+    "Active Enemy Target Button 4"
 
 L.delimiter = ' '
 -- Words
