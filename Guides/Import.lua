2702cfa3978d84162ad534e567c1a6044a76123c
SabreValkyrn <sabre@valkyrnstudios.net>
2022-10-30 21:53:33 -0700

    Update testing data

--- Guides/Import.lua
+++ Guides/Import.lua
@@ -6,20 +6,13 @@ RXPGuides.RegisterGuide([[
 #version 1
 #group Testing
 step
-    .goto Mulgore,44.9,77.1
-    .accept 747 >>Accept The Hunt Begins
-step
-	>> Head into the hut
-    .goto Mulgore,44.2,76.1
-    .accept 752 >>Accept A Humble Task
-step
     .vendor >> Turn-in demo.
     .target Grull Hawkwind
 step
     .vendor >> Group turn-in demo
-    .target Grull Hawkwind
     .target Brave Windfeather
     .target Gart Mistrunner
+    .target Grull Hawkwind
     .target Marjak Keenblade
     .target Moodan Sungrain
 step
@@ -30,8 +23,8 @@ step
     .unitscan Plainstrider
 step
     .vendor >> All the things demo
-    .target Grull Hawkwind
     .target Brave Windfeather
+    .target Grull Hawkwind
     .target Gart Mistrunner
     .target Marjak Keenblade
     .target Moodan Sungrain
