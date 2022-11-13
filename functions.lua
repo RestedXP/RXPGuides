9502ff69d07974f6bb91705825cddaf4e2d5b28a
Semigall <semigall@semigall.live>
2022-11-13 21:12:30 +0200

    New command collectpet
    Added command .collectpet
    Which allows to check if character owns battle pet

--- functions.lua
+++ functions.lua
@@ -40,6 +40,7 @@ events.skill = {"SKILL_LINES_CHANGED", "LEARNED_SPELL_IN_TAB"}
 events.emote = "PLAYER_TARGET_CHANGED"
 events.collectmount = {"COMPANION_LEARNED", "COMPANION_UNLEARNED", "COMPANION_UPDATE", "NEW_PET_ADDED"}
 events.collecttoy = {"TOYS_UPDATED"}
+events.collectpet = {"COMPANION_LEARNED", "COMPANION_UNLEARNED", "COMPANION_UPDATE", "NEW_PET_ADDED"}
 
 events.bankwithdraw = events.bankdeposit
 events.abandon = events.complete
@@ -3978,6 +3979,60 @@ function addon.functions.collecttoy(self, ...)
     local isCollected = PlayerHasToy(element.id)
 
     element.itemName = toyName
+    if element.rawtext then
+        element.tooltipText = element.rawtext
+        element.text = string.format("%s\n%s", element.rawtext, element.itemName)
+    else
+        element.text = string.format("%s", element.itemName)
+        element.tooltipText = element.text
+    end
+    if isCollected then
+        addon.SetElementComplete(self)
+    end
+end
+
+function addon.functions.collectpet(self, ...)
+    if type(self) == "string" then -- on parse
+        local element = {}
+        element.dynamicText = true
+        local text, id = ...
+        id = tonumber(id)
+        if not id then
+            return addon.error(
+                        L("Error parsing guide") .. " "  .. addon.currentGuideName ..
+                           ': No pet npc ID provided\n' .. self)
+        end
+        element.id = id
+        if text and text ~= "" then
+            element.rawtext = text
+            element.tooltipText = element.rawtext
+        else
+            element.text = " "
+        end
+        return element
+    end
+
+    local element = self.element
+
+    -- we need to reset petjournal search, as owned parameter could be received only from journal by index
+    C_PetJournal.SetSearchFilter("")
+    C_PetJournal.SetFilterChecked(LE_PET_JOURNAL_FILTER_COLLECTED, true)
+    C_PetJournal.SetFilterChecked(LE_PET_JOURNAL_FILTER_NOT_COLLECTED, true)
+    C_PetJournal.SetAllPetTypesChecked(true)
+    C_PetJournal.SetAllPetSourcesChecked(true)
+
+    local isCollected = false -- default values so it doesn't break
+    element.itemName = ""
+
+    for index = 1, C_PetJournal.GetNumPets() do
+        local petID, speciesID, owned, cutomName, _, _, _, speciesName, _, _, companionID = C_PetJournal.GetPetInfoByIndex(index);
+        if (companionID == element.id) then -- we found it
+            element.itemName = speciesName
+            isCollected = owned
+            break
+        end
+    end
+
     if element.rawtext then
         element.tooltipText = element.rawtext
         element.text = string.format("%s\n%s", element.rawtext, element.itemName)
