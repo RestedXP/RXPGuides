1045f37eedc07499ee57973dca0fd6b2435bc703
tommysalamiwow <82994911+tommysalamiwow@users.noreply.github.com>
2022-11-22 16:37:30 -0600

    Update Alliance-Worgen_Gilneas.lua

--- Guides/Retail-Starting-Zones/Alliance-Worgen_Gilneas.lua
+++ Guides/Retail-Starting-Zones/Alliance-Worgen_Gilneas.lua
@@ -5,7 +5,7 @@ RXPGuides.RegisterGuide([[
 #subgroup Worgen Starting Zones
 #displayname Worgen_Gilneas
 #name 2Retail-Alliance-Worgen_Gilneas
-#next RestedXP Alliance 10-60\1Retail-Alliance_Darkmoon FaireV
+#next RestedXP Alliance 10-60\1A_Elwynn Forest
 
 <<Worgen !DK
 
