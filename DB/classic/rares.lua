03211d35427c87db7abcbcc4b4ad912ef4f3ac0a
SabreValkyrn <61754281+SabreValkyrn@users.noreply.github.com>
2022-12-03 12:25:08 -0800

    Add Dredd rare data to Classic (#142)

---
+++ DB/classic/rares.lua
@@ -0,0 +1,205 @@
+local _, addon = ...
+
+addon.rares = {
+    ["Elwynn Forest"] = {
+        "Fedfennel", "Gruff Swiftbite", "Morgaine the Sly", "Mother Fang",
+        "Narg the Taskmaster", "Thuros Lightfingers"
+    },
+    ["Durotar"] = {
+        "Captain Flat Tusk", "Death Flayer", "Felweaver Scornn",
+        "Geolord Mottle", "Warlord Kolkanis", "Watch Commander Zalaphil"
+    },
+    ["Mulgore"] = {
+        "Enforcer Emilgund", "Ghost Howl", "Mazzranache", "Sister Hatelash",
+        "Snagglespear", "The Rake"
+    },
+    ["Teldrassil"] = {
+        "Blackmoss the Fetid", "Duskstalker", "Fury Shelda", "Grimmaw",
+        "Threggil", "Uruson"
+    },
+    ["Dun Morogh"] = {
+        "Bjarn", "Edan the Howler", "Gibblewilt", "Great Father Arctikus",
+        "Hammerspine", "Timber"
+    },
+    ["Tirisfal Glades"] = {
+        "Bayne", "Deeb", "Farmer Solliden", "Fellicent's Shade", "Lost Soul",
+        "Muad", "Ressan the Needler", "Sri'skulk", "Tormented Spirit"
+    },
+    ["Westfall"] = {
+        "Brack", "Foe Reaper 4000", "Leprithus", "Master Digger",
+        "Sergeant Brashclaw", "Slark", "Vultros"
+    },
+    ["Loch Modan"] = {
+        "Boss Galgosh", "Emogg the Crusher", "Grizlak", "Large Loch Crocolisk",
+        "Lord Condar", "Magosh", "Shanda the Spinner"
+    },
+    ["Darkshore"] = {
+        "Carnivous the Breaker", "Firecaller Radison", "Flagglemurk the Cruel",
+        "Lady Moongazer", "Lady Vespira", "Licillin", "Lord Sinslayer",
+        "Shadowclaw", "Strider Clutchmother"
+    },
+    ["Silverpine Forest"] = {
+        "Dalaran Spellscribe", "Gorefang", "Krethis Shadowspinner",
+        "Old Vicejaw", "Ravenclaw Regent", "Rot Hide Bruiser", "Snarlmane"
+    },
+    ["The Deadmines"] = {"Miner Johnson", "Edwin VanCleef"},
+    ["Stonetalon Mountains"] = {
+        "Brother Ravenoak", "Foreman Rigger", "Nal'taszar",
+        "Pridewing Patriarch", "Sentinel Amarassan", "Sister Riven",
+        "Sorrow Wing", "Taskmaster Whipfang", "Vengeful Ancient"
+    },
+    ["Redridge Mountains"] = {
+        "Boulderheart", "Chatter", "Kazon", "Ribchaser", "Rohh the Silent",
+        "Seeker Aqualon", "Snarlflare", "Squiddic", "Volchan"
+    },
+    ["Wailing Caverns"] = {"Deviate Faerie Dragon"},
+    ["Duskwood"] = {
+        "Commander Felstrom", "Fenros", "Lord Malathrom", "Lupos", "Naraxis",
+        "Nefaru"
+    },
+    ["Ashenvale"] = {
+        "Akkrilus", "Apothecary Falthis", "Branch Snapper", "Eck'alom",
+        "Lady Vespia", "Mist Howler", "Mugglefin", "Oakpaw", "Prince Raze",
+        "Rorgish Jowl", "Terrowulf Packlord", "Ursol'lok"
+    },
+    ["Wetlands"] = {
+        "Dragonmaw Battlemaster", "Garneg Charskull", "Gnawbone", "Leech Widow",
+        "Ma'ruk Wyrmscale", "Mirelow", "Razormaw Matriarch", "Sludginn"
+    },
+    ["Shadowfang Keep"] = {"Deathsworn Captain"},
+    ["Hillsbrad Foothills"] = {
+        "Big Samras", "Creepthess", "Lady Zephris", "Narillasanz", "Ro'Bark",
+        "Scargil", "Tamra Stormpike"
+    },
+    ["The Stockade"] = {"Bruegal Ironknuckle"},
+    ["The Barrens"] = {
+        "Aean Swiftriver", "Ambassador Bloodrage", "Azzere the Skyblade",
+        "Brokespear", "Brontus", "Captain Gerogg Hammertoe",
+        "Digger Flameforge", "Dishu", "Elder Mystic Razorsnout",
+        "Engineer Whirleygig", "Foreman Grills", "Geopriest Gukk'rok",
+        "Gesharahan", "Hagg Taurenbane", "Hannah Bladeleaf",
+        "Heggin Stonewhisker", "Humar the Pridelord", "Malgin Barleybrew",
+        "Marcus Bel", "Rathorian", "Rocklance", "Silithid Harvester",
+        "Sister Rathtalon", "Sludge Beast", "Snort the Heckler", "Stonearm",
+        "Swiftmane", "Swinegart Spearhide", "Takk the Leaper",
+        "Thora Feathermoon", "Thunderstomp"
+    },
+    ["Thousand Needles"] = {
+        "Achellios the Banished", "Gibblesnik", "Harb Foulmountain",
+        "Heartrazor", "Ironeye the Invincible", "Silithid Ravager", "Vile Sting"
+    },
+    ["Gnomeregan"] = {"Dark Iron Ambassador"},
+    ["Alterac Mountains"] = {
+        "Araga", "Cranky Benj", "Gravis Slipknot", "Jimmy the Bleeder",
+        "Lo'Grosh", "Narillasanz", "Skhowl", "Stone Fury"
+    },
+    ["Desolace"] = {
+        "Accursed Slitherblade", "Cursed Centaur", "Giggler", "Hissperak",
+        "Kaskk", "Prince Kellen"
+    },
+    ["Arathi Highlands"] = {
+        "Darbel Montrose", "Foulbelly", "Kovork", "Molok the Crusher",
+        "Nimar the Slayer", "Prince Nazjak", "Ruul Onestone", "Singer",
+        "Zalas Witherbark"
+    },
+    ["Razorfen Kraul"] = {
+        "Blind Hunter", "Earthcaller Halmgar", "Razorfen Spearhide"
+    },
+    ["Swamp of Sorrows"] = {
+        "Fingat", "Gilmorian", "Lord Captain Wyrmak", "Lost One Chieftain",
+        "Lost One Cook", "Molt Thorn"
+    },
+    ["Scarlet Monastery"] = {
+        "Azshir the Sleepless", "Fallen Champion", "Ironspine"
+    },
+    ["Badlands"] = {
+        "7:XT", "Anathemus", "Broken Tooth", "Rumbler", "Shadowforge Commander",
+        "Siege Golem", "War Golem", "Zaricotl"
+    },
+    ["The Hinterlands"] = {
+        "Grimungous", "Ironback", "Jalinde Summerdrake",
+        "Mith'rethis the Enchanter", "Old Cliff Jumper", "Razortalon",
+        "Retherokk the Berserker", "The Reak", "Witherheart the Stalker",
+        "Zul'arek Hatefowler"
+    },
+    ["Stranglethorn Vale"] = {
+        "Gluggle", "Kurmokk", "Lord Sakrasis", "Mosh'Ogg Butcher", "Rippa",
+        "Roloch", "Scale Belly", "Scarshield Quartermaster", "Verifonix"
+    },
+    ["Tanaris"] = {
+        "Bone Witch", "Cyclok the Mad", "Greater Firebird",
+        "Haarka the Ravenous", "Jin'Zallah the Sandbringer", "Kregg Keelhaul",
+        "Lumbering Horror", "Murderous Blisterpaw", "Omgorn the Lost",
+        "Soriid the Devourer", "Spirit of the Damned", "Warleader Krazzilak"
+    },
+    ["Maraudon"] = {"Meshlok the Harvester"},
+    ["Felwood"] = {
+        "Alshirr Banebreath", "Death Howl", "Dessecus", "Immolatus", "Mongress",
+        "Olm the Wise", "Ragepaw", "The Ongar"
+    },
+    ["Azshara"] = {
+        "Antilos", "Bone Witch", "Gatekeeper Rageroar", "General Fangferror",
+        "Lady Sesspira", "Lumbering Horror", "Magister Hawkhelm",
+        "Master Feardred", "Monnos the Elder", "Scalebeard",
+        "Spirit of the Damned", "The Evalcharr", "Varo'then's Ghost"
+    },
+    ["Un'Goro Crater"] = {
+        "Clutchmother Zavas", "Gruff", "King Mosh", "Ravasaur Matriarch",
+        "Uhk'loc"
+    },
+    ["Searing Gorge"] = {
+        "Faulty War Golem", "Highlord Mastrogonde", "Rekk'tilac", "Scald",
+        "Shleipnarr", "Slave Master Blackheart", "Smoldar"
+    },
+    ["Zul'Farrak"] = {"Dustwraith", "Sandarr Dunereaver", "Zerillis"},
+    ["Western Plaguelands"] = {
+        "Foreman Jerris", "Foreman Marcrid", "Foulmane", "Lord Maldazzar",
+        "Putridius", "Scarlet Executioner", "Scarlet High Clerist",
+        "Scarlet Interrogator", "Scarlet Judge", "Scarlet Smith", "The Husk"
+    },
+    ["Burning Steppes"] = {
+        "Bone Witch", "Deathmaw", "Gorgon'och", "Gruklash", "Hahk'Zor",
+        "Hematos", "Lumbering Horror", "Malfunctioning Reaver",
+        "Spirit of the Damned", "Terrorspark", "Thauris Balgarr", "Volchan"
+    },
+    ["Eastern Plaguelands"] = {
+        "Bone Witch", "Deathspeaker Selendre", "Duggan Wildhammer",
+        "Gish the Unmoving", "Hed'mush the Rotting", "High General Abbendis",
+        "Lord Darkscythe", "Lumbering Horror", "Ranger Lord Hawkspear",
+        "Spirit of the Damned", "Warlord Thresh'jin", "Zul'Brin Warpbranch"
+    },
+    ["Silithus"] = {
+        "Gretheer", "Grubthor", "Huricanian", "Krellack", "Lapress",
+        "Rex Ashil", "Setis", "Twilight Lord Everun", "Zora"
+    },
+    ["Stormwind City"] = {"Sewer Beast"},
+    ["Feralas"] = {
+        "Antilus the Soarer", "Arash-ethis", "Bloodroar the Stalker",
+        "Diamond Head", "Gnarl Leafbrother", "Lady Szallah", "Old Grizzlegut",
+        "Qirot", "Snarler"
+    },
+    ["Blackrock Depths"] = {
+        "Lord Roccor", "Panzor the Invincible", "Pyromancer Loregrain", "Verek",
+        "Warder Stilgiss"
+    },
+    ["Blackrock Spire"] = {
+        "Bannok Grimaxe", "Burning Felguard", "Crystal Fang", "Ghok Bashguud",
+        "Quartermaster Zigris", "Spirestone Battle Lord", "Spirestone Butcher",
+        "Spirestone Lord Magus", "Urok Doomhowl", "Jed Runewatcher"
+    },
+    ["Winterspring"] = {
+        "Azurous", "Bone Witch", "General Colbatann", "Grizzle Snowpaw",
+        "Kashoch the Reaver", "Lady Hederine", "Lumbering Horror",
+        "Mezzir the Howler", "Rak'shiri", "Spirit of the Damned"
+    },
+    ["Dustwallow Marsh"] = {
+        "Brimgore", "Burgle Eye", "Darkmist Widow", "Dart",
+        "Drogoth the Roamer", "Hayoc", "Lord Angler", "Oozeworm", "Ripscale",
+        "The Rot"
+    },
+    ["Blasted Lands"] = {
+        "Akubar the Seer", "Bone Witch", "Clack the Reaver", "Deatheye",
+        "Dreadscorn", "Grunter", "Lumbering Horror", "Magronos the Unyielding",
+        "Mojo the Twisted", "Ravage", "Spirit of the Damned", "Spiteflayer"
+    }
+}
