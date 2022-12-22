fe0bf5c5f5c9b3f63f2a497d192a5a352647e759
Kevin Audebrand <kevin.audebrand@gmail.com>
2022-12-22 10:06:32 +0100

    added basic info for translation

--- locale/frFR.lua
+++ locale/frFR.lua
@@ -1,3 +1,39 @@
+local addonName, addon = ...
+
+-- Return nil ('raw') for non-enUS entries
+local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "frFR", false)
+if not L then return end
+
+_G["BINDING_NAME_" .. "CLICK RXPItemFrameButton1:LeftButton"] =
+    "Active Item Button 1"
+_G["BINDING_NAME_" .. "CLICK RXPItemFrameButton2:LeftButton"] =
+    "Active Item Button 2"
+_G["BINDING_NAME_" .. "CLICK RXPItemFrameButton3:LeftButton"] =
+    "Active Item Button 3"
+_G["BINDING_NAME_" .. "CLICK RXPItemFrameButton4:LeftButton"] =
+    "Active Item Button 4"
+
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_FriendlyButton1:LeftButton"] =
+    "Active Friendly Target Button 1"
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_FriendlyButton2:LeftButton"] =
+    "Active Friendly Target Button 2"
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_FriendlyButton3:LeftButton"] =
+    "Active Friendly Target Button 3"
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_FriendlyButton4:LeftButton"] =
+    "Active Friendly Target Button 4"
+
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_EnemyButton1:LeftButton"] =
+    "Active Enemy Target Button 1"
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_EnemyButton2:LeftButton"] =
+    "Active Enemy Target Button 2"
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_EnemyButton3:LeftButton"] =
+    "Active Enemy Target Button 3"
+_G["BINDING_NAME_" .. "CLICK RXPTargetFrame_EnemyButton4:LeftButton"] =
+    "Active Enemy Target Button 4"
+
+-- Words
+L.words = {["Accept"] = _G.ACCEPT, ["Kill"] = "擊殺"}
+
 
 -- ActiveItemFrame.lua file
 
