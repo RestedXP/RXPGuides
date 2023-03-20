2fa071725457d0fe381f10833f7304e1aad9681a
SabreValkyrn <sabre@valkyrnstudios.net>
2023-03-19 19:03:08 -0700

    Finish BM hunter talents

--- Guides/Talents/wotlk-hunter.lua
+++ Guides/Talents/wotlk-hunter.lua
@@ -17,24 +17,217 @@ if addon.player.class ~= "HUNTER" then return end
 addon.talents.RegisterGuide([[
 #name Beast Mastery
 #next Marksman Raiding
-#minLevel 60
+#minLevel 10
 #maxLevel 80
 level
     .talent 1,1,2,1
     .pettalent 1,1,1
+
 level
     .talent 1,1,2,2
     .pettalent 1,1,2
+
 level
     .talent 1,1,2,3
+
 level
     .talent 1,1,2,4
+
 level
     .talent 1,1,2,5
+
 level
     .talent 1,2,1,1
+
 level
     .talent 1,2,1,2
+
+level
+    .talent 1,2,4,1
+
+level
+    .talent 1,2,4,2
+
+level
+    .talent 1,1,3,1
+
+level
+    .talent 1,3,2,1
+
+level
+    .talent 1,3,3,1
+
+level
+    .talent 1,3,3,2
+
+level
+    .talent 1,3,3,3
+
+level
+    .talent 1,3,3,4
+
+level
+    .talent 1,3,3,5
+
+level
+    .talent 1,4,3,1
+
+level
+    .talent 1,4,3,2
+
+level
+    .talent 1,4,3,3
+
+level
+    .talent 1,4,3,4
+
+level
+    .talent 1,4,3,5
+
+level
+    .talent 1,5,2,1
+
+level
+    .talent 1,5,1,1
+
+level
+    .talent 1,5,4,1
+
+level
+    .talent 1,5,4,2
+
+level
+    .talent 1,6,3,1
+
+level
+    .talent 1,6,3,2
+
+level
+    .talent 1,6,3,3
+
+level
+    .talent 1,6,3,4
+
+level
+    .talent 1,6,1,1
+
+level
+    .talent 1,7,2,1
+
+level
+    .talent 1,6,1,2
+
+level
+    .talent 1,7,1,1
+
+level
+    .talent 1,7,1,2
+
+level
+    .talent 1,7,1,3
+
+level
+    .talent 1,8,1,1
+
+level
+    .talent 1,8,1,2
+
+level
+    .talent 1,8,3,1
+
+level
+    .talent 1,8,3,2
+
+level
+    .talent 1,8,3,3
+
+level
+    .talent 1,9,2,1
+
+level
+    .talent 1,8,3,4
+
+level
+    .talent 1,8,3,5
+
+level
+    .talent 1,9,1,1
+
+level
+    .talent 1,9,1,2
+
+level
+    .talent 1,10,2,1
+
+level
+    .talent 1,10,2,2
+
+level
+    .talent 1,10,2,3
+
+level
+    .talent 1,10,2,4
+
+level
+    .talent 1,10,2,5
+
+level
+    .talent 1,11,2,1
+
+level
+    .talent 2,1,3,1
+
+level
+    .talent 2,1,3,2
+
+level
+    .talent 2,1,3,3
+
+level
+    .talent 2,1,3,4
+
+level
+    .talent 2,1,3,5
+
+level
+    .talent 2,2,3,1
+
+level
+    .talent 2,2,3,2
+
+level
+    .talent 2,2,3,3
+
+level
+    .talent 2,2,3,4
+
+level
+    .talent 2,2,3,5
+
+level
+    .talent 2,3,3,1
+
+level
+    .talent 2,3,4,1
+
+level
+    .talent 2,3,4,2
+
+level
+    .talent 1,5,1,2
+
+level
+    .talent 2,1,2,1
+
+level
+    .talent 2,1,2,2
+
+level
+    .talent 2,1,2,3
+
+level
+    .talent 1,9,1,3
+
 level =80
     .retrain
 ]])
