0901b4cc09c1a88ccd38ffe3e8e7ab772564d884
SabreValkyrn <sabre@valkyrnstudios.net>
2023-04-16 13:30:03 -0700

    Add drowning warning logic

--- Tips.lua
+++ Tips.lua
@@ -2,14 +2,21 @@ local _, addon = ...
 
 if addon.gameVersion > 40000 then return end
 
-local GetTime = _G.GetTime
+local GetTime, GetMirrorTimerProgress = _G.GetTime, _G.GetMirrorTimerProgress
+local UIErrorsFrame = _G.UIErrorsFrame
+local STRING_ENVIRONMENTAL_DAMAGE_DROWNING =
+    _G.STRING_ENVIRONMENTAL_DAMAGE_DROWNING
 
 local L = addon.locale.Get
 
 addon.tips = addon:NewModule("Tips", "AceEvent-3.0")
 
-local session = {checkFrequency = 0.5, checkLast = GetTime()}
-local BREATH_WARNING_THRESHOLD = 0.2
+local session = {
+    checkFrequency = 0.5,
+    checkLast = GetTime(),
+    lastAlert = 0,
+    alertFrequency = 1
+}
 
 function addon.tips:Setup()
     if not addon.settings.db.profile.enableTips then return end
@@ -19,14 +26,15 @@ function addon.tips:Setup()
     self:RegisterEvent("MIRROR_TIMER_START")
     self:RegisterEvent("MIRROR_TIMER_STOP")
 
-    if not addon.settings.db.profile.hideGuideWindow then
-        addon.RXPFrame:HookScript("OnUpdate", self.CheckEvents)
-    elseif addon.settings.db.profile.enableMinimapButton then
+    -- Prioritize minimap button, as main frame can be easily hidden
+    if addon.settings.db.profile.enableMinimapButton then
         addon.settings.minimapFrame:HookScript("OnUpdate", self.CheckEvents)
+    elseif not addon.settings.db.profile.hideGuideWindow then
+        addon.RXPFrame:HookScript("OnUpdate", self.CheckEvents)
         -- elseif addon.tips.frame then
     else
         addon.comms.PrettyPrint(
-            L("No enabled RXP frames for tips functionality"))
+            L("No enabled RXP frames for tips functionality")) -- TODO locale
     end
 end
 
@@ -37,12 +45,13 @@ end
 
 function addon.tips:MIRROR_TIMER_START(_, timerName, value, maxValue, rate)
     if timerName ~= "BREATH" or
-        not addon.settings.db.profile.enableBreathWarning then return end
+        not addon.settings.db.profile.enableDrowningWarning then return end
 
     -- Recovering breath
-    if rate > 0 then session.breath = nil end
-
-    print("MIRROR_TIMER_START", timerName)
+    if rate > 0 then
+        session.breath = nil
+        return
+    end
 
     -- Draining down, event fires with 30 when regaining breath
     session.breath = {value = value, maxValue = maxValue}
@@ -50,9 +59,7 @@ end
 
 function addon.tips:MIRROR_TIMER_STOP(_, timerName)
     if timerName ~= "BREATH" or
-        not addon.settings.db.profile.enableBreathWarning then return end
-
-    print("MIRROR_TIMER_STOP", timerName)
+        not addon.settings.db.profile.enableDrowningWarning then return end
 
     session.breath = nil
     -- TODO
@@ -68,13 +75,19 @@ function addon.tips.CheckEvents()
 
         if session.breath.value == 0 or
             (session.breath.value / session.breath.maxValue) <
-            BREATH_WARNING_THRESHOLD then
-            print("Drowning!", session.breath.value / session.breath.maxValue)
+            addon.settings.db.profile.drowningThreshold then
 
-            -- TODO notify throttling
-            -- TODO sound on drowning
-            -- TODO flash screen edges
-            FlashClientIcon()
+            if GetTime() - session.lastAlert > session.alertFrequency then
+                -- TODO flash screen edges?
+                FlashClientIcon()
+                UIErrorsFrame:AddMessage(STRING_ENVIRONMENTAL_DAMAGE_DROWNING,
+                                         1.0, 0.1, 0.1, session.alertFrequency);
+
+                if addon.settings.db.profile.enableDrowningWarningSound then
+                    PlaySound(_G.SOUNDKIT.RAID_WARNING, "Master")
+                end
+                session.lastAlert = GetTime()
+            end
 
         end
     end
