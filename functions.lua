9f22c9f31b84efce10f99fc940c050f3186b9c4c
Zarant <Zarant@users.noreply.github.com>
2023-04-23 19:36:36 -0300

    objective complete message

--- functions.lua
+++ functions.lua
@@ -518,7 +518,7 @@ function addon.SetElementComplete(self, disable)
         self.element.skip = true
         addon.updateSteps = true
         addon.updateMap = true
-        addon:SendMessage("RXP_OBJECTIVE_COMPLETE",self.element,addon.currentGuide)
+        addon:SendEvent("RXP_OBJECTIVE_COMPLETE",self.element,addon.currentGuide)
     end
     if self.button then
         -- print('----ok',disable)
