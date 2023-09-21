a72d3dd661bb7bc612841fb2cd56042a5b6d69b9
SabreValkyrn <sabre@valkyrnstudios.net>
2023-09-21 13:09:22 -0700

    Update reset guide minLevel

--- Guides/Talents/classic-priest.lua
+++ Guides/Talents/classic-priest.lua
@@ -113,10 +113,10 @@ level -- Divine Fury Rank 5
 
 level -- Inner Focus
 	.talent 1,3,2,1
-	
+
 level -- Unbreakable Will Rank 1
 	.talent 1,1,2,1
-		
+
 level -- Mental Agility Rank 1
 	.talent 1,4,2,1
 
@@ -128,7 +128,7 @@ level -- Mental Agility Rank 3
 
 level -- Mental Agility Rank 4
 	.talent 1,4,2,4
-	
+
 level -- Mental Agility Rank 5
 	.talent 1,4,2,5
 
@@ -253,7 +253,7 @@ level -- Improved Psychic Scream Rank 2
 
 level -- Shadow Reach Rank 1
 	.talent 3,4,3,1
-	
+
 level -- Shadow Reach Rank 2
 	.talent 3,4,3,2
 
@@ -262,9 +262,9 @@ level -- Shadow Reach Rank 2
 
 addon.talents.RegisterGuide([[
 #name Softcore Priest 40-60
-#minLevel 40
+#minLevel 10
 #maxLevel 60
-
+#reset
 
 level -- Spirit Tap Rank 1
 	.talent 3,1,2,1
@@ -313,10 +313,10 @@ level -- Improved Psychic Scream Rank 2
 
 level -- Shadow Reach Rank 1
 	.talent 3,4,3,1
-	
+
 level -- Shadow Reach Rank 2
 	.talent 3,4,3,2
-	
+
 level -- Shadow Reach Rank 3
 	.talent 3,4,3,3
 
@@ -334,10 +334,10 @@ level -- Silence
 
 level -- Shadow Weaving Rank 1
 	.talent 3,4,4,1
-	
+
 level -- Shadow Weaving Rank 2
 	.talent 3,4,4,2
-	
+
 level -- Shadow Weaving Rank 3
 	.talent 3,4,4,3
 
@@ -406,10 +406,10 @@ level -- Meditation Rank 3
 
 level -- Inner Focus
 	.talent 1,3,2,1
-	
+
 level -- Unbreakable Will Rank 1
 	.talent 1,1,2,1
-	
+
 level -- Mental Agility Rank 1
 	.talent 1,4,2,1
 
