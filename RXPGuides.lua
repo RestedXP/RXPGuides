0223f81a1c9891cee3f6c6e85ecaa3bde1d30d13
SabreValkyrn <61754281+SabreValkyrn@users.noreply.github.com>
2023-10-29 18:42:53 -0700

    Persist frame positions in profile (#194)
    * Save active frame positions
    
    * Restore frame positions
    
    * Restore frame positions on profile change

--- RXPGuides.lua
+++ RXPGuides.lua
@@ -742,6 +742,7 @@ function addon:OnEnable()
 
     self:RegisterEvent("PLAYER_ENTERING_WORLD")
     self:RegisterEvent("PLAYER_LEAVING_WORLD")
+    self:RegisterEvent("PLAYER_LOGOUT")
 
     self:RegisterEvent("CALENDAR_UPDATE_EVENT_LIST")
     self:RegisterEvent("ZONE_CHANGED")
@@ -768,8 +769,15 @@ function addon:OnEnable()
         self:RegisterEvent("QUEST_DATA_LOAD_RESULT")
     end
 
-    for _, frame in pairs(addon.enabledFrames) do
+    local point, relativeTo, relativePoint, offsetX, offsetY
+    for frameName, frame in pairs(addon.enabledFrames) do
         if frame.IsFeatureEnabled() then
+            -- Restore saved positions if applicable
+            if addon.settings.profile.framePositions[frameName] then
+                point, relativeTo, relativePoint, offsetX, offsetY = unpack(addon.settings.profile.framePositions[frameName])
+
+                frame:SetPoint(point, relativeTo, relativePoint, offsetX, offsetY)
+            end
             frame:SetShown(addon.settings.profile.showEnabled)
         end
     end
@@ -831,6 +839,24 @@ function addon:PLAYER_ENTERING_WORLD(_, isInitialLogin)
 end
 --addon:LoadGuideTable(addon.defaultGroupHC, addon.defaultGuideHC)
 function addon:PLAYER_LEAVING_WORLD() addon.isHidden = true end
+
+-- Sent when the player logs out or the UI is reloaded, just before SavedVariables are saved
+-- Note, this is only for profile sharing, frames are preserved normally with layout.xml
+function addon:PLAYER_LOGOUT()
+    if not addon.settings.profile.framePositions then
+        addon.settings.profile.framePositions = {}
+    end
+
+    local point, relativeTo, relativePoint, offsetX, offsetY
+    -- Dump enabled frame locations
+    for frameName, frame in pairs(addon.enabledFrames) do
+        -- if frame.IsFeatureEnabled() then
+        point, relativeTo, relativePoint, offsetX, offsetY = frame:GetPoint()
+
+        addon.settings.profile.framePositions[frameName] = {point, relativeTo, relativePoint, offsetX, offsetY}
+    end
+
+end
 
 function addon:CALENDAR_UPDATE_EVENT_LIST()
     -- Required by .dmf
