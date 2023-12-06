3e73ebae299a4e290159784fc2afa0b4854a9cdd
Lardeck <martinw1394@gmail.com>
2023-12-06 16:08:29 +0100

    Add the Demonic Grace, Demonic Tactics, and Soul Siphon Runes

--- Guides/SoD/Warlock.lua
+++ Guides/SoD/Warlock.lua
@@ -110,3 +110,229 @@ step << Undead
 
 
     ]])
+
+RXPGuides.RegisterGuide([[
+#classic
+<< Horde Warlock SoD
+#group RestedXP Rune Guide
+#subgroup Chest
+#name Soul Siphon - 13 (Durotar)
+
+step
+    #completewith next
+    .zone Durotar >>Travel to Durotar
+step
+    .train 403920,1
+    .goto Durotar,48.60,15.28
+    .collect 205020,1 >>Use |T136163:0|t[Drain Soul] on a critter to get a |T134095:0|t[Pure Soul Shard]
+step
+    --Wowhead npc 3203 also possible, maybe better?
+    .train 403920,1
+    >>Use |T136163:0|t[Drain Soul] on |cRXP_ENEMY_Gazz'uz|r (inside the cave) to get a |T134085:0|t[Tainted Soul Shard]. |cRXP_WARN_You don't have to kill him and can drain him from below|r
+    .goto Durotar,51.47,9.73
+    .collect 205019,1
+    .mob Gazz'uz
+step
+    .train 403920,1
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cFF00FF25Darmak Bloodhowl|r
+    .goto Durotar,54.6,41.6
+    .collect 205022,1
+    .skipgossip 208226,1
+    .target Darmak Bloodhowl
+step
+    .use 205022
+    .itemcount 205022,1
+    .train 403920 >>|cRXP_WARN_Use the|r |T134419:0|t[|cRXP_FRIENDLY_Rune of Soul Siphon|r] |cRXP_WARN_to train|r |T136169:0|t[Soul Siphon]
+]])
+
+RXPGuides.RegisterGuide([[
+#classic
+<< Horde Warlock SoD
+#group RestedXP Rune Guide
+#subgroup Chest
+#name Soul Siphon - 9 (Tirisfal Glades)
+
+step
+    #completewith next
+    .zone Tirisfal Glades >>Travel to the Tirisfal Glades
+step
+    #completewith next
+    .train 403920,1
+    .collect 205020,1 >>Use |T136163:0|t[Drain Soul] on a critter to get a |T134095:0|t[Pure Soul Shard]
+step
+    .train 403920,1
+    >>Use |T136163:0|t[Drain Soul] on |cRXP_ENEMY_Maggot Eye|r to get a |T134085:0|t[Tainted Soul Shard]. |cRXP_WARN_You don't have to kill him.|r
+    .goto Tirisfal Glades,58.6,31.6
+    .collect 205019,1
+    .mob Maggot Eye
+step
+    .train 403920,1
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cFF00FF25Denton Bleakway|r in Undercity
+    .goto Undercity,84.2,25.8
+    .collect 205022,1
+    .skipgossip 208682,1
+    .target Denton Bleakway
+step
+    .use 205022
+    .itemcount 205022,1
+    .train 403920 >>|cRXP_WARN_Use the|r |T134419:0|t[|cRXP_FRIENDLY_Rune of Soul Siphon|r] |cRXP_WARN_to train|r |T136169:0|t[Soul Siphon]
+]])
+
+RXPGuides.RegisterGuide([[
+#classic
+<< Horde Warlock SoD
+#group RestedXP Rune Guide
+#subgroup Chest
+#name Demonic Tactics - 4 (Tirisfal Glades)
+
+step
+    #completewith next
+    .zone Tirisfal Glades >>Travel to the Tirisfal Glades
+step
+    .train 416009,1
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tInteract with the |cFF00FF25Dead Acolyte|r. Open |T133625:0|t[Acolyte's Knapsack]
+    .goto Tirisfal Glades,58.6,31.6
+    .use 205364
+    .collect 205181,1
+    .collect 205184,1
+    .skipgossip 208927,1
+    .mob Dead Acolyte
+step
+    .train 416009,1
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cFF00FF25Rupert Boch|r upstairs in the inn.
+    .goto Tirisfal Glades,61.6,52.4
+    .collect 205182,1
+    .skipgossip 2127,2
+    .target Rupert Boch
+step
+    .train 416009,1
+    >>Use the |T133447:0|t[Powerless Artifact] next to the rune stone. |cRXP_WARN_Afterwards you have 10 minutes to get to Undercity (look at your debuff)|r
+    .goto Tirisfal Glades,58.6,31.6
+    .use 205182
+    .collect 205183,1
+step
+    .train 416009,1
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cFF00FF25Carentin Halgar|r in Undercity
+    .goto Undercity,85.0,25.6
+    .collect 205215,1
+    .skipgossip 5675,1
+    .target Carendin Halgar
+step
+    .use 205215
+    .itemcount 205215,1
+    .train 416009 >>|cRXP_WARN_Use the|r |T134419:0|t[|cRXP_FRIENDLY_Rune of Tactics|r] |cRXP_WARN_to train|r |T136150:0|t[Demonic Tactics]
+]])
+
+RXPGuides.RegisterGuide([[
+#classic
+<< Horde Warlock SoD
+#group RestedXP Rune Guide
+#subgroup Legs
+#name Demonic Grace - 8 (Durotar)
+
+step
+    #completewith DemonicGraceDurotarTome
+    .zone Durotar >>Travel to Durotar
+step
+    #completewith DemonicGraceDurotarSkull
+    >>Kill |cRXP_ENEMY_Makrura|r. Loot them for the |T133571:0|t[Makrura Legs]
+    .collect 207732,1
+    .mob Makrura Clacker
+    .mob Makrura Shellhide
+step
+    #label DemonicGraceDurotarTome
+    >>Kill the |cRXP_ENEMY_Hexed Trolls|r and the |cRXP_ENEMY_Voodoo Trolls|r. Loot them for the |T133733:0|t[Ominous Tome]
+    .goto Durotar,67.2,85.6
+    .collect 207731,1
+    .mob Hexed Troll
+    .mob Voodoo Troll
+step
+    #label DemonicGraceDurotarSkull
+    >>Kill the |cRXP_ENEMY_Kul Tiran Humans|r. Loot them for the |T133730:0|t[Kul Tiran Skull]
+    .goto Durotar,58.6,56.0
+    .collect 207733,1
+    .mob Kul Tiras Sailor
+    .mob Kul Tiras Marine
+step
+    #loop
+    .goto Durotar,61.0,43.0,50,0
+    .goto Durotar,60.8,70.6,50,0
+    .goto Durotar,51.6,84.6,50,0
+    .goto Durotar,60.8,70.6,50,0
+    >>Kill |cRXP_ENEMY_Makrura|r. Loot them for the |T133571:0|t[Makrura Legs]
+    .collect 207732,1
+    .mob Makrura Clacker
+    .mob Makrura Shellhide
+step
+    .train 425477,1
+    *|cRXP_WARN_CAREFUL: Others can tag your demon which means that you would have to farm the reagents again|r
+    >>Enter the Sewers. Use the |T133733:0|t[Ominous Tome] at the Summoning Circle. Kill |cRXP_WARN_THE ELITE (maybe look for help)|r |cRXP_ENEMY_Soboz|r. Loot him for the |T134419:0|t[|cRXP_FRIENDLY_Rune of Grace|r]
+    .goto Durotar,67.45,87.83
+    .collect 204912,1
+    .mob Soboz
+step
+    .use 204912
+    .itemcount 204912,1
+    .train 425477 >>|cRXP_WARN_Use the|r |T134419:0|t[|cRXP_FRIENDLY_Rune of Grace|r] |cRXP_WARN_to train|r |T236293:0|t[Demonic Grace]
+]])
+
+RXPGuides.RegisterGuide([[
+#classic
+<< Horde Warlock SoD
+#group RestedXP Rune Guide
+#subgroup Legs
+#name Demonic Grace - 8 (Tirisfal Glades)
+
+step
+    .train 425477,1
+    #completewith DemonicGraceTirisfalGladesTome
+    .zone Tirisfal Glades >>Travel to Tirisfal Glades
+step
+    .train 425477,1
+    #completewith DemonicGraceTirisfalGladesBlood
+    >>Kill |cRXP_ENEMY_Darkhounds|r. Loot them for the |T133726:0|t[Wolf Jawbone]
+    .collect 204907,1
+    .mob Cursed Darkhound
+    .mob Decrepit Darkhound
+    .mob Ravenous Darkhound
+step
+    .train 425477,1
+    #label DemonicGraceTirisfalGladesTome
+    >>Kill the |cRXP_ENEMY_Darkeye Bonecasters|r. Loot them for the |T133733:0|t[Ominous Tome]
+    .goto Tirisfal Glades,47.6,36.4
+    .collect 207974,1
+    .mob Darkeye Bonecaster
+step
+    .train 425477,1
+    #label DemonicGraceTirisfalGladesBlood
+    >>Kill the |cRXP_ENEMY_Rot Hides|r. Loot them for the |T133730:0|t[Gnoll Blood]
+    .goto Tirisfal Glades,58.6,34.6
+    .collect 204906,1
+    .mob Rot Hide Mongrel
+    .mob Rot Hide Gnoll
+    .mob Rot Hide Graverobber
+step
+    .train 425477,1
+    #loop
+    .goto Tirisfal Glades,73.4,52.8,50,0
+    .goto Tirisfal Glades,59.4,60.2,50,0
+    .goto Tirisfal Glades,44.4,58.4,50,0
+    .goto Tirisfal Glades,42.0,43.0,50,0
+    >>Kill |cRXP_ENEMY_Darkhounds|r. Loot them for the |T133726:0|t[Wolf Jawbone]
+    .collect 204907,1
+    .mob Cursed Darkhound
+    .mob Decrepit Darkhound
+    .mob Ravenous Darkhound
+step
+    .train 425477,1
+    *|cRXP_WARN_CAREFUL: Others can tag your demon which means that you would have to farm the reagents again|r
+    >>Enter the Sewers. Use the |T133733:0|t[Ominous Tome] at the Summoning Circle. Kill |cRXP_WARN_THE ELITE (maybe look for help)|r |cRXP_ENEMY_Soboz|r. Loot him for the |T134419:0|t[|cRXP_FRIENDLY_Rune of Grace|r]
+    .goto Undercity,15.1,31.3,20,0
+    .goto Undercity,24.11,41.59
+    .collect 204912,1
+    .mob Soboz
+step
+    .use 204912
+    .itemcount 204912,1
+    .train 425477 >>|cRXP_WARN_Use the|r |T134419:0|t[|cRXP_FRIENDLY_Rune of Grace|r] |cRXP_WARN_to train|r |T236293:0|t[Demonic Grace]
+]])
