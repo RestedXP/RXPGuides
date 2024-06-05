cf15410a874f83d650565e2d46ee18ea98e15086
Shiekrunner <shiekrunner@gmail.com>
2024-06-05 16:33:06 +0200

    Added Daillies

--- Guides/Retail Pandaria Remix Free/Retail-Pandaria-Remix-Dailies.lua
+++ Guides/Retail Pandaria Remix Free/Retail-Pandaria-Remix-Dailies.lua
@@ -370,6 +370,68 @@ RXPGuides.RegisterGuide([[
 #internal
 
 
+step
+    .goto 422,54.25,35.78
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Kaz'tik the Manipulator|r
+    .accept 31268 >>Accept A Little Brain Work
+    .target Kaz'tik the Manipulator
+    .accept 31024 >>Accept Kunchong Treats
+step
+    .goto 422,54.36,35.94
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Rik'kal the Dissector|r
+    .accept 31271 >>Accept Bad Genes
+    .target Rik'kal the Dissector
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Korven the Prime|r
+    .accept 31270 >>Accept The Fight Against Fear
+    .target Korven the Prime
+    .accept 31269 >>Accept The Scale-Lord
+step
+    .goto 422,59.13,53.27
+    >>Kill |cRXP_ENEMY_Horrorscale Scorpid|r
+    .complete 31270,1 --15/15 Horrorscale Scorpid slain
+    .mob Horrorscale Scorpid
+step
+    .goto 422,61.74,54.07
+    .complete 31024,1 --4/4 Meaty Turtle Haunch
+step
+    .goto 422,62.55,66.55
+    >>Kill |cRXP_ENEMY_Ik'thik Genemancer|r
+    .complete 31271,1 --3/3 Ik'thik Genemancer slain
+    .mob Ik'thik Genemancer
+step
+    .goto 422,61.67,70.77
+    .complete 31268,1 --3/3 Amber-Encrusted Brain
+step
+    .goto 422,60.90,71.95
+    >>Kill |cRXP_ENEMY_Ik'thik Egg-Drone|r
+    .complete 31271,2 --6/6 Ik'thik Egg-Drone slain
+    .mob Ik'thik Egg-Drone
+step
+    .goto 422,66.40,66.21
+    >>Kill |cRXP_ENEMY_Mistblade Scale-Lord|r
+    .complete 31269,1 --1/1 Mistblade Scale-Lord slain
+    .mob Mistblade Scale-Lord
+step
+    .goto 422,54.26,35.78
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Kaz'tik the Manipulator|r
+    .turnin 31024 >>Turn in Kunchong Treats
+    .target Kaz'tik the Manipulator
+    .turnin 31268 >>Turn in A Little Brain Work
+
+step
+    .goto 422,54.36,35.93
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Rik'kal the Dissector|r
+    .turnin 31271 >>Turn in Bad Genes
+    .target Rik'kal the Dissector
+step
+    .goto 422,54.30,36.10
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Korven the Prime|r
+    .turnin 31269 >>Turn in The Fight Against Fear
+    .target Korven the Prime
+    .turnin 31270 >>Turn in The Fight Against Fear
+
+
+
 ]])
 
 -- August Celestial Dailies: Jade Forest
@@ -381,6 +443,85 @@ RXPGuides.RegisterGuide([[
 #internal
 
 
+step
+    .goto 371,53.90,61.96
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Elder Sage Tai-Feng|r
+    .accept 30066 >>Accept Hidden Power
+    .target Elder Sage Tai-Feng
+    .accept 30065 >>Accept Arrows of Fortune
+    .accept 30006 >>Accept The Darkness Around Us
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Elder Sage Storm-Sing|r
+    .turnin 80429 >>Turn in Aid the August Celestials
+    .target Elder Sage Storm-Sing
+step
+    .goto 371,52.63,55.65
+    .complete 30065,1 --8/8 Defender's Arrow
+step
+    .goto 371,53.73,56.63
+    .complete 30066,1 --6/6 Ancient Mantras delivered
+step
+    .goto 371,55.08,57.07
+    >>Kill |cRXP_ENEMY_Sha invaders|r
+    .complete 30006,1 --15/15 Sha invaders slain
+    .mob Sha invaders
+step
+    .goto 371,53.90,61.95
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Elder Sage Tai-Feng|r
+    .turnin 30006 >>Turn in Arrows of Fortune
+    .target Elder Sage Tai-Feng
+    .turnin 30066 >>Turn in Arrows of Fortune
+
+    .turnin 30065 >>Turn in Arrows of Fortune
+
+    .accept 30067 >>Accept The Shadow of Doubt
+step
+    .goto 371,57.53,62.36
+    >>Kill |cRXP_ENEMY_Shadow of Doubt|r
+    .complete 30067,1 --1/1 Shadow of Doubt slain
+    .mob Shadow of Doubt
+step
+    .goto 371,53.91,61.96
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Elder Sage Tai-Feng|r
+    .turnin 30067 >>Turn in The Shadow of Doubt
+    .target Elder Sage Tai-Feng
+
+
+    SET2
+    step
+    .goto 371,53.92,61.92
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Elder Sage Tai-Feng|r
+    .accept 30066 >>Accept Hidden Power
+    .target Elder Sage Tai-Feng
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Elder Sage Storm-Sing|r
+    .accept 30064 >>Accept Saving the Sutras
+    .target Elder Sage Storm-Sing
+    .accept 30063 >>Accept Behind the Masks
+step
+    .goto 371,53.89,54.57
+    .complete 30063,1 --8/8 Mask of Doubt
+    .complete 30066,1 --6/6 Ancient Mantras delivered
+step
+    .goto 371,54.93,51.21
+    .complete 30064,1 --6/6 Ancient Sutra
+step
+    .goto 371,53.89,61.91
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Elder Sage Storm-Sing|r
+    .turnin 30063 >>Turn in Behind the Masks
+    .target Elder Sage Storm-Sing
+    .turnin 30064 >>Turn in Saving the Sutras
+
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Elder Sage Tai-Feng|r
+    .turnin 30066 >>Turn in Hidden Power
+    .target Elder Sage Tai-Feng
+    .accept 30068 >>Accept Flames of the Void
+step
+    .goto 371,55.57,54.36
+    .complete 30068,1 --6/6 Void Flames doused
+step
+    .goto 371,53.91,61.94
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Elder Sage Tai-Feng|r
+    .turnin 30068 >>Turn in Flames of the Void
+    .target Elder Sage Tai-Feng
 ]])
 
 -- August Celestial Dailies: Kunlai
@@ -390,6 +531,62 @@ RXPGuides.RegisterGuide([[
 #group RestedXP Panda Remix
 #name X) August Celestial Daillies
 #internal
+
+
+step
+    .goto 379,67.24,55.89
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Xuen|r
+    .turnin 80431 >>Turn in Aid the August Celestials
+    .target Xuen
+    .accept 31517 >>Accept Contending With Bullies
+    .accept 30879 >>Accept Round 1: Brewmaster Chani
+step
+    .goto 379,71.09,55.93
+    >>Kill |cRXP_ENEMY_Shonuf|r
+    .complete 31517,1 --1/1 Shonuf slain
+    .mob Shonuf
+step
+    .goto 379,70.96,51.82
+    .complete 30879,1 --1/1 Defeat Brewmaster Chani
+step
+    .goto 379,70.28,51.26
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Xuen|r
+    .turnin 30879 >>Turn in Round 1: Brewmaster Chani
+    .target Xuen
+    .accept 30881 >>Accept Round 2: Clever Ashyo & Ken-Ken
+    .turnin 31517 >>Turn in Contending With Bullies
+
+step
+    .goto 379,71.67,45.35
+    .complete 30881,2 --1/1 Defeat Ken-Ken
+    .complete 30881,1 --1/1 Defeat Clever Ashyo
+step
+    .goto 379,71.76,44.89
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Xuen|r
+    .turnin 30881 >>Turn in Round 2: Clever Ashyo & Ken-Ken
+    .target Xuen
+    .accept 30883 >>Accept Round 3: The Wrestler
+step
+    .goto 379,66.74,46.53
+    .complete 30883,1 --1/1 Defeat The Wrestler
+step
+    .goto 379,66.39,46.34
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Xuen|r
+    .turnin 30883 >>Turn in Round 3: The Wrestler
+    .target Xuen
+    .accept 30907 >>Accept Round 4: The P.U.G.
+step
+    .goto 379,69.02,43.76
+    .complete 30907,3 --1/1 Defeat Tankiss
+    .complete 30907,1 --1/1 Defeat Hackiss
+    .complete 30907,2 --1/1 Defeat Healiss
+step
+    .goto 379,68.49,44.58
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Xuen|r
+    .turnin 30907 >>Turn in Round 4: The P.U.G.
+    .target Xuen
+    .turnin 80431 >>Turn in Aid the August Celestials
+
 
 
 ]])
@@ -539,7 +736,7 @@ RXPGuides.RegisterGuide([[
 
 ]])
 
--- Anglers Dailies
+-- Landfall Daillies Alliance
 RXPGuides.RegisterGuide([[
 #df
 #version 1
@@ -598,4 +795,165 @@ step
     .turnin 32151 >>Turn in Tower Defense
 ---SET2---
 
+step
+    .goto 418,89.82,32.41
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Amber Kearnen|r
+    .accept 32451 >>Accept Send A Message
+    .target Amber Kearnen
+
+
+step
+    .goto 418,16.46,79.20
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Sky Admiral Rogers|r
+    .turnin 32451 >>Turn in Send A Message
+    .target Sky Admiral Rogers
+    .accept 32142 >>Accept We Will Rock You
+    .accept 32146 >>Accept Hammer to Fall
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Tinkmaster Overspark|r
+    .accept 32143 >>Accept A Kind of Magic
+    .target Tinkmaster Overspark
+step
+    .goto 418,16.66,78.53
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Mishka|r
+    .accept 32144 >>Accept Under Pressure
+    .target Mishka
+step
+    .goto 418,11.66,67.84
+    .complete 32143,1 --1/1 Eliminate the Beachhead Demolishers
+step
+    .goto 418,8.38,65.87
+    .complete 32144,1 --8/8 Horde Supply Crates Destroyed
+step
+    .goto 418,8.65,61.42
+    >>Kill |cRXP_ENEMY_Domination Orc|r
+    .complete 32142,1 --10/10 Domination Orc slain
+    .mob Domination Orc
+step
+    .goto 418,8.78,60.67
+    >>Kill |cRXP_ENEMY_Heavy Mook|r
+    .complete 32146,1 --5/5 Heavy Mook slain
+    .mob Heavy Mook
+step
+    .goto 418,10.89,60.91
+    .complete 32143,2 --1/1 Eliminate the Horde Wolves
+step
+    .goto 418,12.64,56.60
+    .complete 32143,3 --1/1 Destroy the Horde Bomb Stockpile
+step
+    .goto 418,12.64,56.60
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Shiekrunners|r
+    .turnin 32143 >>Turn in A Kind of Magic
+    .target Shiekrunners
+    .accept 32145 >>Accept Don't Lose Your Head
+step
+    .goto 418,13.29,56.25
+    >>Kill |cRXP_ENEMY_Chief Bombgineer Snicklefritz|r
+    .complete 32145,3 --1/1 Chief Bombgineer Snicklefritz slain
+    .mob Chief Bombgineer Snicklefritz
+step
+    .goto 418,13.40,57.85
+    >>Kill |cRXP_ENEMY_Stone Guard Ruk'Ra|r
+    .complete 32145,2 --1/1 Stone Guard Ruk'Ra slain
+    .mob Stone Guard Ruk'Ra
+step
+    .goto 418,10.38,58.00
+    >>Kill |cRXP_ENEMY_Or'Dac|r
+    .complete 32145,1 --1/1 Or'Dac slain
+    .mob Or'Dac
+step
+    .goto 418,16.68,78.58
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Mishka|r
+    .turnin 32144 >>Turn in Under Pressure
+    .target Mishka
+step
+    .goto 418,16.47,79.19
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Sky Admiral Rogers|r
+    .turnin 32142 >>Turn in We Will Rock You
+    .target Sky Admiral Rogers
+    .turnin 32146 >>Turn in Hammer to Fall
+
+step
+    .goto 418,16.52,79.42
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Tinkmaster Overspark|r
+    .turnin 32145 >>Turn in Don't Lose Your Head
+    .target Tinkmaster Overspark
+
+    ---SET1---
+
+step
+    .goto 418,68.31,43.45
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_John "Big Hook" Marsock|r
+    .accept 30753 >>Accept Jumping the Shark
+    .target John "Big Hook" Marsock
+step
+    .goto 418,67.94,45.22
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Fisherman Haito|r
+    .accept 30586 >>Accept Jagged Abalone
+    .target Fisherman Haito
+step
+    .goto 418,68.91,37.96
+    .complete 30586,1 --9/9 Jagged Abalone Meat
+step
+    .goto 418,68.33,43.50
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_John "Big Hook" Marsock|r
+    .turnin 30753 >>Turn in Jumping the Shark
+    .target John "Big Hook" Marsock
+step
+    .goto 418,67.93,45.24
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Fisherman Haito|r
+    .turnin 30586 >>Turn in Jagged Abalone
+    .target Fisherman Haito
+---SET2---
+
+step
+    .goto 418,89.70,33.09
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Marshal Troteman|r
+    .accept 32116 >>Accept Priorities, People!
+    .target Marshal Troteman
+step
+    .goto 419,30.20,27.65
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Hilda Hornswaggle|r
+    .turnin 32116 >>Turn in Priorities, People!
+    .target Hilda Hornswaggle
+    .accept 32347 >>Accept Eviction Notice
+    .accept 32346 >>Accept Oi Ain't Afraid o' No Ghosts!
+    .accept 32122 >>Accept Ogudei's Lieutenants
+    .accept 32121 >>Accept The Spirit Trap
+    .accept 32115 >>Accept Shackles of the Past
+step
+    .goto 421,57.97,3.19
+    .complete 32115,1 --14/14 Troubled Slave Spirits released
+step
+    .goto 421,62.17,1.69
+    >>Kill |cRXP_ENEMY_Spiritbound Mogu|r
+    .complete 32346,1 --12/12 Spiritbound Mogu slain
+    .mob Spiritbound Mogu
+step
+    .goto 421,81.92,96.31
+    .complete 32121,1 --3/3 Spirit Trap destroyed
+step
+    .goto 420,68.66,78.64
+    >>Kill |cRXP_ENEMY_Gen-li, Twister of Words|r
+    .complete 32122,1 --1/1 Gen-li, Twister of Words slain
+    .mob Gen-li, Twister of Words
+    >>Kill |cRXP_ENEMY_Meng-do, Strength of Mountains|r
+    .complete 32122,2 --1/1 Meng-do, Strength of Mountains slain
+    .mob Meng-do, Strength of Mountains
+step
+    .goto 421,60.11,52.81
+    >>Kill |cRXP_ENEMY_Reliquary|r
+    .complete 32347,1 --10/10 Reliquary slain
+    .mob Reliquary
+step
+    .goto 419,30.07,27.46
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Hilda Hornswaggle|r
+    .turnin 32347 >>Turn in Eviction Notice
+    .target Hilda Hornswaggle
+    .turnin 32115 >>Turn in Shackles of the Past
+
+    .turnin 32346 >>Turn in Oi Ain't Afraid o' No Ghosts!
+
+    .turnin 32121 >>Turn in The Spirit Trap
+
+    .turnin 32122 >>Turn in Ogudei's Lieutenants
 ]])
