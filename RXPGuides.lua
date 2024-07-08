a50b90eced8e084d96f236de6d1d530fee6ed9cb
Zarant <Zarant@users.noreply.github.com>
2024-07-08 09:48:50 -0300

    Added missing nil check

--- RXPGuides.lua
+++ RXPGuides.lua
@@ -91,7 +91,7 @@ end
 local GetAddOnMetadata = C_AddOns and C_AddOns.GetAddOnMetadata or _G.GetAddOnMetadata
 addon.release = GetAddOnMetadata(addonName, "Version")
 addon.title = GetAddOnMetadata(addonName, "Title")
-local cacheVersion = 24
+local cacheVersion = 25
 local L = addon.locale.Get
 
 if string.match(addon.release, 'project') then
@@ -1721,7 +1721,7 @@ end
 
 function addon.stepLogic.HardcoreCheck(step)
     local hc = addon.settings.profile.hardcore
-    local hcserver = C_GameRules and C_GameRules.IsHardcoreActive()
+    local hcserver = C_GameRules and C_GameRules.IsHardcoreActive and C_GameRules.IsHardcoreActive()
     if step.softcoreserver and hcserver or step.hardcoreserver and not hcserver then return false end
     if step.softcore and hc or step.hardcore and not hc then return false end
     return true
