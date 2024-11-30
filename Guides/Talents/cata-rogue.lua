f8c038947080c5d692fd085c860836503b9f9d2e
SabreValkyrn <61754281+SabreValkyrn@users.noreply.github.com>
2024-11-30 13:11:13 -0800

    Add Subtlety Rogue

--- Guides/Talents/cata-rogue.lua
+++ Guides/Talents/cata-rogue.lua
@@ -259,3 +259,132 @@ level -- Quickening
 level -- Quickening
 	.talent 3,1,1,2
 ]])
+
+addon.talents.RegisterGuide([[
+#name Subtlety Rogue 10-85
+#minLevel 10
+#maxLevel 85
+
+level -- Improved Ambush
+	.talent 3,1,2,1
+
+level -- Improved Ambush
+	.talent 3,1,2,2
+
+level -- Nightstalker
+	.talent 3,1,1,1
+
+level -- Improved Ambush
+	.talent 3,1,2,3
+
+level -- Nightstalker
+	.talent 3,1,1,2
+
+level -- Opportunity
+	.talent 3,2,3,1
+
+level -- Opportunity
+	.talent 3,2,3,2
+
+level -- Opportunity
+	.talent 3,2,3,3
+
+level -- Initiative
+	.talent 3,2,3,1
+
+level -- Initiative
+	.talent 3,2,3,2
+
+level -- Hemorrhage
+	.talent 3,3,1,1
+
+level -- Find Weakness
+	.talent 3,3,2,1
+
+level -- Find Weakness
+	.talent 3,3,2,2
+
+level -- Energetic Recovery
+	.talent 3,3,1,1
+
+level -- Energetic Recovery
+	.talent 3,3,1,2
+
+level -- Premeditation
+	.talent 3,4,2,1
+
+level -- Honor Among Thieves
+	.talent 3,4,1,1
+
+level -- Honor Among Thieves
+	.talent 3,4,1,2
+
+level -- Honor Among Thieves
+	.talent 3,4,1,3
+
+level -- Energetic Recovery
+	.talent 3,3,1,3
+
+level -- Preparation
+	.talent 3,5,2,1
+
+level -- Sanguinary Vein
+	.talent 3,5,3,1
+
+level -- Sanguinary Vein
+	.talent 3,5,3,2
+
+level -- Elusiveness
+	.talent 3,2,1,1
+
+level -- Elusiveness
+	.talent 3,2,1,2
+
+level -- Slaughter from the Shadows
+	.talent 3,6,2,1
+
+level -- Slaughter from the Shadows
+	.talent 3,6,2,2
+
+level -- Slaughter from the Shadows
+	.talent 3,6,2,3
+
+level -- Serrated Blades
+	.talent 3,6,3,1
+
+level -- Serrated Blades
+	.talent 3,6,3,2
+
+level -- Shadow Dance
+	.talent 3,7,2,1
+
+level -- Deadly Momentum
+	.talent 1,1,1,1
+
+level -- Deadly Momentum
+	.talent 1,1,1,2
+
+level -- Lethality
+	.talent 1,1,3,1
+
+level -- Lethality
+	.talent 1,1,3,2
+
+level -- Lethality
+	.talent 1,1,3,3
+
+level -- Precision
+	.talent 2,1,3,1
+
+level -- Precision
+	.talent 2,1,3,2
+
+level -- Precision
+	.talent 2,1,3,3
+
+level -- Improved Recuperate
+	.talent 2,1,1,1
+
+level -- Improved Recuperate
+	.talent 2,1,1,2
+]])
