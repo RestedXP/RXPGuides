adad51cbe13580c261da1e0b47109b3029d645d8
SabreValkyrn <61754281+SabreValkyrn@users.noreply.github.com>
2024-12-08 10:56:55 -0800

    Add Demonology Warlock

--- Guides/Talents/cata-warlock.lua
+++ Guides/Talents/cata-warlock.lua
@@ -130,3 +130,132 @@ level -- Demonic Aegis
 level -- Mana Feed
 	.talent 2,2,3,1
 ]])
+
+addon.talents.RegisterGuide([[
+#name Demonology Warlock 10-85
+#minLevel 10
+#maxLevel 85
+
+level -- Demonic Embrace
+	.talent 2,1,1,1
+
+level -- Demonic Embrace
+	.talent 2,1,1,2
+
+level -- Demonic Embrace
+	.talent 2,1,1,3
+
+level -- Dark Arts
+	.talent 2,1,2,1
+
+level -- Dark Arts
+	.talent 2,1,2,2
+
+level -- Dark Arts
+	.talent 2,1,2,3
+
+level -- Fel Synergy
+	.talent 2,1,3,1
+
+level -- Demonic Rebirth
+	.talent 2,2,1,1
+
+level -- Demonic Rebirth
+	.talent 2,2,1,2
+
+level -- Mana Feed
+	.talent 2,2,2,1
+
+level -- Mana Feed
+	.talent 2,2,2,2
+
+level -- Demonic Aegis
+	.talent 2,2,3,1
+
+level -- Demonic Aegis
+	.talent 2,2,3,2
+
+level -- Master Summoner
+	.talent 2,2,4,1
+
+level -- Master Summoner
+	.talent 2,2,4,2
+
+level -- Impending Doom
+	.talent 2,3,1,1
+
+level -- Impending Doom
+	.talent 2,3,1,2
+
+level -- Impending Doom
+	.talent 2,3,1,3
+
+level -- Molten Core
+	.talent 2,4,1,1
+
+level -- Molten Core
+	.talent 2,4,1,2
+
+level -- Molten Core
+	.talent 2,4,1,3
+
+level -- Hand of Gul'dan
+	.talent 2,4,2,1
+
+level -- Aura of Foreboding
+	.talent 2,4,3,1
+
+level -- Aura of Foreboding
+	.talent 2,4,3,2
+
+level -- Ancient Grimoire
+	.talent 2,5,1,1
+
+level -- Ancient Grimoire
+	.talent 2,5,1,2
+
+level -- Inferno
+	.talent 2,5,2,1
+
+level -- Decimation
+	.talent 2,5,3,1
+
+level -- Decimation
+	.talent 2,5,3,2
+
+level -- Cremation
+	.talent 2,6,2,1
+
+level -- Cremation
+	.talent 2,6,2,2
+
+level -- Demonic Pact
+	.talent 2,6,3,1
+
+level -- Metamorphosis
+	.talent 2,7,2,1
+
+level -- Improved Corruption
+	.talent 1,1,3,1
+
+level -- Improved Corruption
+	.talent 1,1,3,2
+
+level -- Improved Corruption
+	.talent 1,1,3,3
+
+level -- Improved Life Tap
+	.talent 1,1,2,1
+
+level -- Improved Life Tap
+	.talent 1,1,2,2
+
+level -- Doom and Gloom
+	.talent 1,1,1,1
+
+level -- Doom and Gloom
+	.talent 1,1,1,2
+
+level -- Siphon Life
+	.talent 1,2,3,1
+]])
