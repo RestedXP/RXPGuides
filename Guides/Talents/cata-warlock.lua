c18b471f5dac9ac1f0764d02f7a11c389566aab6
SabreValkyrn <61754281+SabreValkyrn@users.noreply.github.com>
2024-12-08 11:09:57 -0800

    Add Destruction Warlock

--- Guides/Talents/cata-warlock.lua
+++ Guides/Talents/cata-warlock.lua
@@ -259,3 +259,132 @@ level -- Doom and Gloom
 level -- Siphon Life
 	.talent 1,2,3,1
 ]])
+
+addon.talents.RegisterGuide([[
+#name Destruction Warlock 10-85
+#minLevel 10
+#maxLevel 85
+
+level -- Bane
+	.talent 3,1,1,1
+
+level -- Bane
+	.talent 3,1,1,2
+
+level -- Bane
+	.talent 3,1,1,3
+
+level -- Shadow and Flame
+	.talent 3,1,3,1
+
+level -- Shadow and Flame
+	.talent 3,1,3,2
+
+level -- Shadow and Flame
+	.talent 3,1,3,3
+
+level -- Improved Immolate
+	.talent 3,1,3,1
+
+level -- Improved Immolate
+	.talent 3,1,3,2
+
+level -- Emberstorm
+	.talent 3,2,2,1
+
+level -- Emberstorm
+	.talent 3,2,2,2
+
+level -- Improved Soul Fire
+	.talent 3,3,1,1
+
+level -- Improved Soul Fire
+	.talent 3,3,1,2
+
+level -- Backdraft
+	.talent 3,3,2,1
+
+level -- Backdraft
+	.talent 3,3,2,2
+
+level -- Backdraft
+	.talent 3,3,2,3
+
+level -- Shadowburn
+	.talent 3,3,3,1
+
+level -- Burning Embers
+	.talent 3,4,1,1
+
+level -- Burning Embers
+	.talent 3,4,1,2
+
+level -- Nether Ward
+	.talent 3,4,4,1
+
+level -- Backlash
+	.talent 3,4,3,1
+
+level -- Backlash
+	.talent 3,4,3,2
+
+level -- Soul Leech
+	.talent 3,4,2,1
+
+level -- Soul Leech
+	.talent 3,4,2,2
+
+level -- Fire and Brimstone
+	.talent 3,5,2,1
+
+level -- Fire and Brimstone
+	.talent 3,5,2,2
+
+level -- Fire and Brimstone
+	.talent 3,5,2,3
+
+level -- Bane of Havoc
+	.talent 3,6,3,1
+
+level -- Empowered Imp
+	.talent 3,6,1,1
+
+level -- Empowered Imp
+	.talent 3,6,1,2
+
+level -- Shadowfury
+	.talent 3,5,3,1
+
+level -- Chaos Bolt
+	.talent 3,7,2,1
+
+level -- Dark Arts
+	.talent 2,1,2,1
+
+level -- Dark Arts
+	.talent 2,1,2,2
+
+level -- Dark Arts
+	.talent 2,1,2,3
+
+level -- Improved Corruption
+	.talent 1,1,3,1
+
+level -- Improved Corruption
+	.talent 1,1,3,2
+
+level -- Improved Corruption
+	.talent 1,1,3,3
+
+level -- Fel Synergy
+	.talent 2,1,3,1
+
+level -- Improved Life Tap
+	.talent 1,1,2,1
+
+level -- Improved Life Tap
+	.talent 1,1,2,2
+
+level -- Backlash
+	.talent 3,4,3,3
+]])
