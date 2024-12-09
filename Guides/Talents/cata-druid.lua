9485fcce6c6d24539209780c3995205ed1d7c388
SabreValkyrn <61754281+SabreValkyrn@users.noreply.github.com>
2024-12-08 17:22:57 -0800

    Add Feral Druid

--- Guides/Talents/cata-druid.lua
+++ Guides/Talents/cata-druid.lua
@@ -130,3 +130,133 @@ level -- Perseverance
 level -- Perseverance
 	.talent 3,2,1,2
 ]])
+
+addon.talents.RegisterGuide([[
+#name Feral Druid 10-85
+#minLevel 10
+#maxLevel 85
+
+level -- Feral Swiftness
+	.talent 2,1,1,1
+
+level -- Feral Swiftness
+	.talent 2,1,1,2
+
+level -- Furor
+	.talent 2,1,2,1
+
+level -- Furor
+	.talent 2,1,2,2
+
+level -- Furor
+	.talent 2,1,2,3
+
+level -- Feral Aggression
+	.talent 2,2,3,1
+
+level -- Feral Aggression
+	.talent 2,2,3,2
+
+level -- Predatory Strikes
+	.talent 2,1,3,1
+
+level -- Predatory Strikes
+	.talent 2,1,3,2
+
+level -- Primal Fury
+	.talent 2,2,3,1
+
+level -- Primal Fury
+	.talent 2,2,3,2
+
+level -- King of the Jungle
+	.talent 2,3,1,1
+
+level -- King of the Jungle
+	.talent 2,3,1,2
+
+level -- King of the Jungle
+	.talent 2,3,1,3
+
+level -- Feral Charge
+	.talent 2,3,2,1
+
+level -- Leader of the Pack
+	.talent 2,4,2,1
+
+level -- Stampede
+	.talent 2,3,3,1
+
+level -- Stampede
+	.talent 2,3,3,2
+
+level -- Fury Swipes
+	.talent 2,2,2,1
+
+level -- Fury Swipes
+	.talent 2,2,2,2
+
+level -- Fury Swipes
+	.talent 2,2,2,3
+
+level -- Survival Instincts
+	.talent 2,5,2,1
+
+level -- Infected Wounds
+	.talent 2,2,1,1
+
+level -- Infected Wounds
+	.talent 2,2,1,2
+
+level -- Thick Hide
+	.talent 2,3,4,1
+
+level -- Thick Hide
+	.talent 2,3,4,2
+
+level -- Thick Hide
+	.talent 2,3,4,3
+
+level -- Rend and Tear
+	.talent 2,6,2,1
+
+level -- Rend and Tear
+	.talent 2,6,2,2
+
+level -- Rend and Tear
+	.talent 2,6,2,3
+
+level -- Berserk
+	.talent 2,7,2,1
+
+level -- Heart of the Wild
+	.talent 3,1,4,1
+
+level -- Heart of the Wild
+	.talent 3,1,4,2
+
+level -- Heart of the Wild
+	.talent 3,1,4,3
+
+level -- Endless Carnage
+	.talent 2,5,3,1
+
+level -- Endless Carnage
+	.talent 2,5,3,2
+
+level -- Natural Shapeshifter
+	.talent 3,1,2,1
+
+level -- Natural Shapeshifter
+	.talent 3,1,2,2
+
+level -- Master Shapeshifter
+	.talent 3,2,2,1
+
+level -- Primal Madness
+	.talent 2,5,1,1
+
+level -- Primal Madness
+	.talent 2,5,1,2
+]])
+
