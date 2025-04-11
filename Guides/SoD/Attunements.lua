0b18403da2d262873614a13629f22679f2471082
Ksuper2 <166393896+Ksuper2@users.noreply.github.com>
2025-04-11 20:36:04 +0200

    Scarlet Enclave Introduction questline (SoD)

--- Guides/SoD/Attunements.lua
+++ Guides/SoD/Attunements.lua
@@ -210,40 +210,182 @@ step
     .target Harrison Jones
 ]])
 
---UNCOMMENT BELOW IF NEEDED
---RXPGuides.RegisterGuide([[
---#classic
---#season 2
---#group RestedXP Endgame Guides
---#subgroup Attunements
---#name Scarlet Enclave Attunement
+RXPGuides.RegisterGuide([[
+#classic
+#season 2
+#group RestedXP Endgame Guides
+#subgroup Attunements
+#name Scarlet Enclave Introduction
 
---step
---    #completewith next
---    .subzone 2268 >> Travel to the |cRXP_LOOT_Light's Hope Chapel|r in |cRXP_PICK_Eastern Plaguelands|r
---step
---    .goto Eastern Plaguelands,81.6,57.8
---    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Leonid Barthalomew the Revered|r inside the chapel
---    .accept 87459 >> Accept Scarlet Activities
---    .target Leonid Barthalomew the Revered
---step
---    #completewith next
---    .zone Tirisfal Glades >> Travel to Tirisfal Glades
---step
---    .goto Tirisfal Glades,81,58
---    >>Click on the |cRXP_PICK_Ball and Chain|r outside of the small tent. It will spawn a |cRXP_ENEMY_Scarlet Infiltrator|r who will attack you
---    >>Kill him and loot for the |T133471:0|t[Orders from the High General]
---    .complete 87459,1 --Orders from the High General
---    .mob Scarlet Infiltrator
---step
---    #completewith next
---    .subzone 2268 >> Return to the |cRXP_LOOT_Light's Hope Chapel|r in |cRXP_PICK_Eastern Plaguelands|r
---step
---    .goto Eastern Plaguelands,81.6,57.8
---    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Leonid Barthalomew the Revered|r inside the chapel
---   .turnin 87459 >> Turn in Scarlet Activities
---    .target Leonid Barthalomew the Revered
---step
---    +|cRXP_WARN_Congratulations, you're now attuned to the Scarlet Enclave!|r
-
---]])
+step
+   #completewith next
+   .subzone 2268 >> Travel to the |cRXP_LOOT_Light's Hope Chapel|r in |cRXP_PICK_Eastern Plaguelands|r
+step
+   .goto Eastern Plaguelands,81.73,57.84
+   >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Leonid Barthalomew the Revered|r inside the chapel
+   .accept 87459 >> Accept Scarlet Activities
+   .target Leonid Barthalomew the Revered
+step
+    #optional
+    #completewith next
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Jessica Chambers|r
+    .home >> If you want to|cRXP_WARN_ you can set your hearthstone here|r. This will help you return from Tirisfal Glades quicker
+    .target Jessica Chambers
+step << Alliance
+    .goto Eastern Plaguelands,81.64,59.29
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Khaelyn Steelwing|r outside
+    .fly Chillwind >> Fly to Western Plaguelands
+    .target Khaelyn Steelwing
+    .isOnQuest 87459
+step << Horde
+    .goto Eastern Plaguelands,80.23,57.02
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Georgia|r outside
+    .target Georgia
+    .fly Undercity >> Fly to the Undercity
+    .isOnQuest 87459
+step << Horde
+    .goto Tirisfal Glades,61.85,66.59,60 >> Exit Undercity
+    .isOnQuest 87459
+step << Alliance
+   #completewith next
+   .goto Tirisfal Glades,84.85,70.57
+   .zone Tirisfal Glades >> Travel to Tirisfal Glades
+   .isOnQuest 87459
+step
+   .goto Tirisfal Glades,81.76,58.06
+   >>Head north to the scarlet camp << Alliance
+   >>Head east to the scarlet camp << Horde
+   >>Click on the |cRXP_PICK_Ball and Chain|r outside of the small tent. It will spawn an |cRXP_WARN_elite|r |cRXP_ENEMY_Scarlet Infiltrator|r who will attack you
+   >>Kill and |cRXP_LOOT_loot|r him for the |T133471:0|t[Orders from the High General]
+   .complete 87459,1 --Orders from the High General
+   .mob Scarlet Infiltrator
+   .isOnQuest 87459
+step
+   #completewith next
+   .subzone 2268 >> Return to the |cRXP_LOOT_Light's Hope Chapel|r in |cRXP_PICK_Eastern Plaguelands|r
+   .isOnQuest 87459
+step
+   .goto Eastern Plaguelands,81.73,57.84
+   >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Leonid Barthalomew the Revered|r inside the chapel
+    .turnin 87459 >> Turn in Scarlet Activities
+    .accept 87493 >> Accept Unrest at Tyr's Hand
+    .target Leonid Barthalomew the Revered
+step
+    .goto Eastern Plaguelands,67.8,83.2
+    >>Travel south to the scarlet base camp
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Commander Beatrix|r
+    .turnin 87493 >> Turn in Unrest at Tyr's Hand
+    .accept 87497 >> Accept The Schism
+    .target Commander Beatrix
+step
+    .goto Eastern Plaguelands,67.8,83.2
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Commander Beatrix|r and complete her dialogue
+    .complete 87497,1
+    .skipgossip
+    .target Commander Beatrix
+step
+   .goto Eastern Plaguelands,81.73,57.84
+   >>Return to the Light's Hope Chapel
+   >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Leonid Barthalomew the Revered|r inside
+    .turnin 87497 >> Turn in The Schism
+    .accept 87498 >> Accept The Scarlet Redemption
+    .target Leonid Barthalomew the Revered
+step
+    .goto Eastern Plaguelands,67.8,83.2
+    >>Return to the scarlet base camp
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Commander Beatrix|r
+    .turnin 87498 >> Turn in The Scarlet Redemption
+    .target Commander Beatrix
+step
+    .goto Eastern Plaguelands,68.25,82.70
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Scarlet Inquisitor Caldoran|r
+    .accept 87502 >> Accept Gathering Intelligence
+    .target Scarlet Inquisitor Caldoran
+step
+    .goto Eastern Plaguelands,68.18,82.43
+    +|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Inquisitor Jociphine|r |cRXP_WARN_to receive a temporary scarlet disguise|r
+    .aura 1231929 --Scarlet Illusion
+    .skipgossip
+    .target Inquisitor Jociphine
+step
+    >>Enter Tyr's Hand and travel to the Cathedral in the center
+    .goto Eastern Plaguelands,85.27,83.98
+    .complete 87502,1 --Scout the Cathedral in Tyr's Hand: 1/1 
+step
+    .goto Eastern Plaguelands,89.76,81.41,30 >>Take the left path leading into New Avalon
+step
+    >>Head towards the keep in New Avalon
+    >>|cRXP_WARN_Be careful of |r|cRXP_ENEMY_Scarlet Bloodhounds|r |cRXP_WARN_they will see through your disguise!|r
+    >>You can use |T132328:0|t[Track Beasts] to help keep track of the hounds position << Hunter
+    .goto Eastern Plaguelands,96.66,83.06
+    .complete 87502,3 --Scout the Keep in New Avalon: 1/1
+    .unitscan Sarlet Bloodhound
+step
+    >>Head towards the Mage Tower
+    >>|cRXP_WARN_Be careful of |r|cRXP_ENEMY_Scarlet Bloodhounds|r |cRXP_WARN_they will see through your disguise!|r
+    >>You can use |T132328:0|t[Track Beasts] to help keep track of the hounds position << Hunter
+    .goto Eastern Plaguelands,98.14,87.88
+    .complete 87502,2 --Scout the Mage Tower in New Avalon: 1/1
+    .unitscan Sarlet Bloodhound
+step
+    >>Return to the scarlet base camp
+    .goto Eastern Plaguelands,68.25,82.70
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Scarlet Inquisitor Caldoran|r
+    .turnin 87502 >> Turn in Gathering Intelligence
+    .accept 87506 >> Accept Weakening the Defenses
+    .target Scarlet Inquisitor Caldoran
+    .unitscan Sarlet Bloodhound
+step
+    #optional
+    #completewith next
+    >>|cRXP_WARN_You can now unlock the permanent Scarlet Disguise by collecting 4 pieces of a craftable scarlet set then using the provided|r |T134503:0|t[|cFF0070FFScarlet Insignia|r]
+    >>You should be able to find those on the auction house. It's not mandatory but will make traversing New Avalon in the future much easier
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Inquisitor Jociphine|r
+    .accept 90510 >> Accept New Avalon
+    .use 237020 --Scarlet Insignia
+step
+    >>Enter Tyr's Hand and look for |cRXP_ENEMY_Scarlet Siege Commanders|r 
+    >>|cRXP_WARN_They can be found more commonly near siege weapons or on the walls|r
+    .complete 87506,1 --Scarlet Siege Commander (3)
+    .target Scarlet Siege Commander
+step
+    .goto Eastern Plaguelands,67.8,83.2
+    >>Return to the scarlet base camp
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Commander Beatrix|r
+    .turnin 87506 >> Turn in Weakening the Defenses
+    .target Commander Beatrix
+step
+    .goto Eastern Plaguelands,67.8,83.2
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to the |cRXP_FRIENDLY_Argent Emissary|r
+    .accept 87508 >> Accept Report to Lord Tyrosus
+step
+    >>Travel back to the Light's Hope Chapel
+    .goto Eastern Plaguelands,81.74,57.97
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to the |cRXP_FRIENDLY_Lord Maxwell Tyrosus|r inside
+    .turnin 87508 >> Turn in Report to Lord Tyrosus
+    .accept 87509 >> Accept The Wrath of the Dawn
+    .target Lord Maxwell Tyrosus
+step
+    .goto Eastern Plaguelands,67.8,83.2
+    >>Return to the scarlet base camp
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Commander Beatrix|r
+    .skipgossip
+    .complete 87509,1 --Report back to Commander Beatrix
+step
+    .goto Eastern Plaguelands,67.8,83.2
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Commander Beatrix|r
+    .turnin 87509 >> Turn in The Wrath of the Dawn
+    .accept 87516 >> Accept Decapitation Strike
+    .target Commander Beatrix
+step
+    .goto Eastern Plaguelands,68.36,87.58
+    >>This quest requires you to kill |cRXP_ENEMY_Balnazzar|r |cRXP_WARN_the first boss in the new Scarlet Enclave raid|r
+    >>Find a raid group and happy hunting!
+    .complete 87516,1
+step
+    >>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to the |cRXP_FRIENDLY_Lord Maxwell Tyrosus|r inside the prison you get moved to after defeating Balnazzar in in the raid
+    .turnin 87516 >> Turn in Decapitation Strike
+    .target Lord Maxwell Tyrosus
+step
+    +|cRXP_WARN_Congratulations you have completed the Scarlet Enclave introductory questline!|r
+]])
