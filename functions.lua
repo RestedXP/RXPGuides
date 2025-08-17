28aa44c602aa412db8d1919d38925022a3f5c852
Zarant <Zarant@users.noreply.github.com>
2025-08-17 11:38:33 -0300

    Fixed issue with step skips

--- functions.lua
+++ functions.lua
@@ -827,10 +827,11 @@ function addon.SetElementComplete(self, disable, skipIfInactive)
     addon.UpdateMap()
     if active and GetTime() - addon.lastStepUpdate > 1 then
         addon:QueueMessage("RXP_OBJECTIVE_COMPLETE",element,addon.currentGuide)
-        if element.OnComplete then
-            --print('onc',element.step.index)
-            element.OnComplete(element)
-        end
+    end
+
+    if element.OnComplete and active then
+        --print('onc',element.step.index)
+        element.OnComplete(element)
     end
 
     if self.button then
