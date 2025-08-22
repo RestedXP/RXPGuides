e1a60c8b2f0b6c0272f14994df4f038ec3797749
SabreValkyrn <61754281+SabreValkyrn@users.noreply.github.com>
2025-08-21 17:55:30 -0700

    Add Shadow Priest

--- Guides/Talents/tbc-priest.lua
+++ Guides/Talents/tbc-priest.lua
@@ -3,9 +3,9 @@ local _, addon = ...
 if addon.player.class ~= "PRIEST" or addon.game ~= "TBC" then return end
 
 addon.talents.RegisterGuide([[
-#name Priest 10-39
+#name Shadow Priest (Wand) 10-40
 #minLevel 10
-#maxLevel 39
+#maxLevel 40
 
 level -- Wand Specialization Rank 1
 	.talent 1,1,3,1
@@ -21,30 +21,6 @@ level -- Wand Specialization Rank 4
 
 level -- Wand Specialization Rank 5
 	.talent 1,1,3,5
-
-level -- Improved Power Word: Shield Rank 1
-	.talent 1,2,3,1
-
-level -- Improved Power Word: Shield Rank 2
-	.talent 1,2,3,2
-
-level -- Improved Power Word: Shield Rank 3
-	.talent 1,2,3,3
-
-level -- Improved Power Word: Fortitude Rank 1
-	.talent 1,2,2,1
-
-level -- Improved Power Word: Fortitude Rank 1
-	.talent 1,2,2,2
-
-level -- Meditation Rank 1
-	.talent 1,3,3,1
-
-level -- Meditation Rank 2
-	.talent 1,3,3,2
-
-level -- Meditation Rank 3
-	.talent 1,3,3,3
 
 level -- Spirit Tap Rank 1
 	.talent 3,1,2,1
@@ -70,11 +46,11 @@ level -- Shadow Focus Rank 2
 level -- Shadow Focus Rank 3
 	.talent 3,2,3,3
 
-level -- Shadow Focus Rank 4
-	.talent 3,2,3,4
+level -- Improved Shadow Word Pain Rank 1
+	.talent 3,2,2,1
 
-level -- Shadow Focus Rank 5
-	.talent 3,2,3,5
+level -- Improved Shadow Word Pain Rank 2
+	.talent 3,2,2,2
 
 level -- Mind Flay
 	.talent 3,3,3,1
@@ -85,11 +61,11 @@ level -- Improved Mind Blast Rank 1
 level -- Improved Mind Blast Rank 2
 	.talent 3,3,2,2
 
-level -- Improved Psychic Scream Rank 1
-	.talent 3,3,1,1
+level -- Improved Mind Blast Rank 3
+	.talent 3,3,2,3
 
-level -- Improved Psychic Scream Rank 2
-	.talent 3,3,1,2
+level -- Improved Mind Blast Rank 4
+	.talent 3,3,2,4
 
 level -- Shadow Reach Rank 1
 	.talent 3,4,3,1
@@ -97,13 +73,35 @@ level -- Shadow Reach Rank 1
 level -- Shadow Reach Rank 2
 	.talent 3,4,3,2
 
+level -- Shadow Weaving Rank 1
+	.talent 3,4,4,1
+
+level -- Shadow Weaving Rank 2
+	.talent 3,4,4,2
+
+level -- Shadow Weaving Rank 3
+	.talent 3,4,4,3
+
+level -- Focused Mind Rank 1
+	.talent 3,5,4,1
+
+level -- Focused Mind Rank 2
+	.talent 3,5,4,2
+
+level -- Focused Mind Rank 3
+	.talent 3,5,4,3
+
+level -- Shadow Weaving Rank 4
+	.talent 3,4,4,4
+
+level -- Vampiric Embrace
+	.talent 3,5,2,1
 ]])
 
-
 addon.talents.RegisterGuide([[
-#name Priest 40-60
+#name Shadow Priest 10-70
 #minLevel 10
-#maxLevel 60
+#maxLevel 70
 #reset
 
 level -- Spirit Tap Rank 1
@@ -130,11 +128,11 @@ level -- Shadow Focus Rank 2
 level -- Shadow Focus Rank 3
 	.talent 3,2,3,3
 
-level -- Shadow Focus Rank 4
-	.talent 3,2,3,4
+level -- Improved Shadow Word Pain Rank 1
+	.talent 3,2,2,1
 
-level -- Shadow Focus Rank 5
-	.talent 3,2,3,5
+level -- Improved Shadow Word Pain Rank 2
+	.talent 3,2,2,2
 
 level -- Mind Flay
 	.talent 3,3,3,1
@@ -145,32 +143,17 @@ level -- Improved Mind Blast Rank 1
 level -- Improved Mind Blast Rank 2
 	.talent 3,3,2,2
 
-level -- Improved Psychic Scream Rank 1
-	.talent 3,3,1,1
-
-level -- Improved Psychic Scream Rank 2
-	.talent 3,3,1,2
-
-level -- Shadow Reach Rank 1
-	.talent 3,4,3,1
-
-level -- Shadow Reach Rank 2
-	.talent 3,4,3,2
-
-level -- Shadow Reach Rank 3
-	.talent 3,4,3,3
-
 level -- Improved Mind Blast Rank 3
 	.talent 3,3,2,3
 
 level -- Improved Mind Blast Rank 4
 	.talent 3,3,2,4
 
-level -- Vampiric Embrace
-	.talent 3,5,2,1
+level -- Shadow Reach Rank 1
+	.talent 3,4,3,1
 
-level -- Silence
-	.talent 3,5,1,1
+level -- Shadow Reach Rank 2
+	.talent 3,4,3,2
 
 level -- Shadow Weaving Rank 1
 	.talent 3,4,4,1
@@ -180,6 +163,21 @@ level -- Shadow Weaving Rank 2
 
 level -- Shadow Weaving Rank 3
 	.talent 3,4,4,3
+
+level -- Focused Mind Rank 1
+	.talent 3,5,4,1
+
+level -- Focused Mind Rank 2
+	.talent 3,5,4,2
+
+level -- Focused Mind Rank 3
+	.talent 3,5,4,3
+
+level -- Shadow Weaving Rank 4
+	.talent 3,4,4,4
+
+level -- Vampiric Embrace
+	.talent 3,5,2,1
 
 level -- Darkness Rank 1
 	.talent 3,6,3,1
@@ -199,11 +197,35 @@ level -- Darkness Rank 5
 level -- Shadowform
 	.talent 3,7,2,1
 
-level -- Shadow Weaving Rank 4
-	.talent 3,4,4,4
-
 level -- Shadow Weaving Rank 5
 	.talent 3,4,4,5
+
+level -- Improved Mind Blast Rank 5
+	.talent 3,3,2,5
+
+level -- Shadow Power Rank 1
+	.talent 3,7,3,1
+
+level -- Shadow Power Rank 2
+	.talent 3,7,3,2
+
+level -- Misery Rank 1
+	.talent 3,8,3,1
+
+level -- Misery Rank 2
+	.talent 3,8,3,2
+
+level -- Misery Rank 3
+	.talent 3,8,3,3
+
+level -- Misery Rank 4
+	.talent 3,8,3,4
+
+level -- Misery Rank 5
+	.talent 3,8,3,5
+
+level -- Vampiric Touch
+	.talent 3,9,2,1
 
 level -- Wand Specialization Rank 1
 	.talent 1,1,3,1
@@ -220,12 +242,6 @@ level -- Wand Specialization Rank 4
 level -- Wand Specialization Rank 5
 	.talent 1,1,3,5
 
-level -- Improved Power Word: Fortitude Rank 1
-	.talent 1,2,2,1
-
-level -- Improved Power Word: Fortitude Rank 1
-	.talent 1,2,2,2
-
 level -- Improved Power Word: Shield Rank 1
 	.talent 1,2,3,1
 
@@ -234,6 +250,12 @@ level -- Improved Power Word: Shield Rank 2
 
 level -- Improved Power Word: Shield Rank 3
 	.talent 1,2,3,3
+
+level -- Martyrdom Rank 1
+	.talent 1,3,4,1
+
+level -- Martyrdom Rank 2
+	.talent 1,3,4,2
 
 level -- Meditation Rank 1
 	.talent 1,3,3,1
@@ -247,13 +269,21 @@ level -- Meditation Rank 3
 level -- Inner Focus
 	.talent 1,3,2,1
 
-level -- Unbreakable Will Rank 1
-	.talent 1,1,2,1
+level -- Blackout Rank 1
+	.talent 3,1,3,1
 
-level -- Mental Agility Rank 1
-	.talent 1,4,2,1
+level -- Blackout Rank 2
+	.talent 3,1,3,2
 
-level -- Mental Agility Rank 2
-	.talent 1,4,2,2
+level -- Blackout Rank 3
+	.talent 3,1,3,3
 
+level -- Blackout Rank 4
+	.talent 3,1,3,4
+
+level -- Blackout Rank 5
+	.talent 3,1,3,5
+
+level -- Shadow Power Rank 3
+	.talent 3,7,3,3
 ]])
