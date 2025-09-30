1502b37419adbf7be9ca1d219cdfda8e36079e94
SabreValkyrn <61754281+SabreValkyrn@users.noreply.github.com>
2025-09-29 19:31:17 -0700

    Remove unused function

--- CptStadicsVendorTreasures.lua
+++ CptStadicsVendorTreasures.lua
@@ -670,12 +670,6 @@ function GetTableLength(T)
     return count
 end
 
-function GetDistance(x1, y1, x2, y2)
-    local dx = x1 - x2
-    local dy = y1 - y2
-    return math.sqrt(dx * dx + dy * dy)
-end
-
 function addon.VendorTreasures.UpdatePins()
     if not addon.settings.profile.enableVendorTreasure then
         Frame:HideWorldMapPins()
