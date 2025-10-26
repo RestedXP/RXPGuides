3972ca5933082ae8ea5c309b4545060631e9d1b4
SabreValkyrn <61754281+SabreValkyrn@users.noreply.github.com>
2025-10-25 21:38:25 -0700

    Fix Talent Guide disablement bug
    race condition when intermediate spec and over maxLevel for one of them

--- Talents.lua
+++ Talents.lua
@@ -125,6 +125,7 @@ local function buildTalentGuidesMenu()
                 invalidReason = "> " .. guide.maxLevel
                 disabled = true
             else
+                disabled = true
                 invalidReason = nil
             end
 
