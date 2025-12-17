6f28803fdec849ad86903a09eb273cf394f9a4ec
Zarant <Zarant@users.noreply.github.com>
2025-12-17 15:37:20 -0300

    Updated localization script

--- Scripts/dump_locale.py
+++ Scripts/dump_locale.py
@@ -1,39 +1,73 @@
+
 import re
 import os
 import glob
+import sys
+
+path = os.path.dirname(sys.argv[0])
+os.chdir(path)
+dir_path = os.path.realpath(os.path.join(path, os.path.pardir))
+
 
 # Get all *.lua files from RXPGuides directory
-dir_path = 'PATH_TO_RXP_ROOT\*.lua'
-files_list = glob.glob(dir_path)
+files_list = glob.glob(dir_path + '\\**\\*.lua',recursive=True)
 
 # Function to dump locale if files
 # (locale format: "L["string"])
+
+exceptions = [
+    r'\\libs\\',
+    r'\\Guides\\',
+    r'\\locale\\',
+    r'\\Scripts\\',
+]
+
+def check_exceptions(dir):
+    for entry in exceptions:
+        if re.search(entry,dir):
+            return True
+    return False
+
 def locale_dump(input_text):
-    pattern = re.compile("L\(\s*\"(.*?)\"\)|L\(\'(.*?)\'\)")
+    pattern = re.compile("L\(\s*\"(.*?)\"\)|L\(\'(.*?)\'\)|L\'(.*?)\'|L\"(.*?)\"")
     return pattern.finditer(input_text)
 
 # Looping through files list and extracting locale
 seen = set() # Check for duplicates
+
+
+with open ("new_locale.lua", 'wt') as f:
+    f.write('--')
+
+
 for file in files_list:
     file_path = file
+    if check_exceptions(file):
+        continue
     with open (file_path, 'rt') as lua_file:
         file_name = os.path.basename(file_path)
-        content = lua_file.read()
-        locales = locale_dump(content)
-        with open('new_locale.lua', 'a') as f:
-            f.write('\n')
-            f.write(f"-- {file_name} file") # Write each filename
-            f.write('\n\n')
-            for match in locales:
-                if match.group(1):
-                    h = hash(match.group(1))
-                    if h not in seen:
-                        f.write(f"L[\"{match.group(1)}\"] = \"\"")
-                        seen.add(h)
-                        f.write('\n')
-                if match.group(2):
-                    h = hash(match.group(2))
-                    if h not in seen:
-                        f.write(f"L[\"{match.group(2)}\"] = \"\"")
-                        seen.add(h)
-                        f.write('\n')
+        print(file_path)
+        try:
+            content = lua_file.read()
+            locales = locale_dump(content)
+
+            with open('new_locale.lua', 'a') as f:
+                f.write('\n')
+                p = os.path.relpath(file_path, start=dir_path)
+                f.write(f"-- {p} file") # Write each filename
+                f.write('\n\n')
+                for match in locales:
+                    if match.group(1):
+                        h = hash(match.group(1))
+                        if h not in seen:
+                            f.write(f"L[\"{match.group(1)}\"] = \"\"")
+                            seen.add(h)
+                            f.write('\n')
+                    if match.group(2):
+                        h = hash(match.group(2))
+                        if h not in seen:
+                            f.write(f"L[\"{match.group(2)}\"] = \"\"")
+                            seen.add(h)
+                            f.write('\n')
+        except:
+            print('Error')
