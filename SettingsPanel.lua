25f14a9550f3a0a62b973ae22924874b53429abf
SabreValkyrn <61754281+SabreValkyrn@users.noreply.github.com>
2025-12-20 17:33:48 -0800

    Inventory current functionality

--- SettingsPanel.lua
+++ SettingsPanel.lua
@@ -4282,4 +4282,17 @@ function addon.settings.LaunchConfigurator()
 
     if isFirstRun and addon.startHardcoreIntroUI then return addon.startHardcoreIntroUI() end
 
+    -- Set softcore or hardcore
+    -- Proxmity Scanning (deprecated)
+    -- Auction House
+    -- Group Quests (turn into Group Settings)
+    -- Dungeon selector
+    --- Dungeons with bonuses and weighting
+    --- Recommendations
+
+    if addon.settings.profile.hardcore then
+        addon:LoadGuideTable(addon.defaultGroupHC, addon.defaultGuideHC)
+    else
+        addon:LoadGuideTable(addon.defaultGroup, addon.defaultGuide)
+    end
 end
