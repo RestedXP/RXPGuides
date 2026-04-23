3abfaefd4422bfbc3cfb60356594c0b8990da9e9
SabreValkyrn <61754281+SabreValkyrn@users.noreply.github.com>
2026-04-22 17:31:08 -0700

    Add RXPV2ScrollFrame and theme propagation

--- UI/V2/Core.lua
+++ UI/V2/Core.lua
@@ -1,6 +1,9 @@
 local addonName, addon = ...
 
 local locale = _G.GetLocale()
+local pairs, assert, type = pairs, assert, type
+local min, max, floor = math.min, math.max, math.floor
+local CreateFrame, UIParent = CreateFrame, UIParent
 
 -- AceAddon doesn't exist yet
 local AceGUI = LibStub("AceGUI-3.0")
@@ -15,8 +18,231 @@ function addon.ui.v2:Initialize()
     -- Locales.lua loads after this file is included
     L = addon.locale.Get
 
+    self:RegisterRXPV2ScrollBar()
     self:RegisterRXPV2CurrentStepFrame()
     self:RegisterRXPV2CurrentStepItem()
+end
+
+local function updateTheme(this, payload)
+    if not payload then return end
+
+    print("Updating", this.type)
+    if payload.showBackground ~= nil and this.frame.SetBackdrop then
+        if payload.showBackground then
+            this.frame:SetBackdrop(payload.edge or addon.RXPFrame.backdrop.edge)
+            this.frame:SetBackdropColor(unpack(payload.background or addon.colors.background))
+        else
+            this.frame:ClearBackdrop()
+        end
+    end
+
+    if payload.updateChildren then
+        for _, child in pairs(this.children or {}) do
+            if child.UpdateTheme then
+                child:UpdateTheme(payload)
+            end
+        end
+    end
+end
+
+function addon.ui.v2:RegisterRXPV2ScrollBar()
+    --[[-----------------------------------------------------------------------------
+    ScrollFrame Container
+    Plain container that scrolls its content and doesn't grow in height.
+    -------------------------------------------------------------------------------]]
+    local Type, Version = "RXPV2ScrollFrame", 1
+    if not AceGUI or (AceGUI:GetWidgetVersion(Type) or 0) >= Version then return end
+
+    --[[-----------------------------------------------------------------------------
+    Support functions
+    -------------------------------------------------------------------------------]]
+    local function FixScrollOnUpdate(frame)
+        frame:SetScript("OnUpdate", nil)
+        frame.obj:FixScroll()
+    end
+
+    --[[-----------------------------------------------------------------------------
+    Scripts
+    -------------------------------------------------------------------------------]]
+    local function ScrollFrame_OnMouseWheel(frame, value) frame.obj:MoveScroll(value) end
+
+    local function ScrollFrame_OnSizeChanged(frame) frame:SetScript("OnUpdate", FixScrollOnUpdate) end
+
+    local function ScrollBar_OnScrollValueChanged(frame, value) frame.obj:SetScroll(value) end
+
+    --[[-----------------------------------------------------------------------------
+    Methods
+    -------------------------------------------------------------------------------]]
+    local methods = {
+        ["OnAcquire"] = function(this)
+            this:SetScroll(0)
+            this.scrollframe:SetScript("OnUpdate", FixScrollOnUpdate)
+        end,
+
+        ["OnRelease"] = function(this)
+            this.status = nil
+            for k in pairs(this.localstatus) do this.localstatus[k] = nil end
+            this.scrollframe:SetPoint("BOTTOMRIGHT")
+            this.scrollbar:Hide()
+            this.scrollBarShown = nil
+            this.content.height, this.content.width, this.content.original_width = nil, nil, nil
+        end,
+
+        ["SetScroll"] = function(this, value)
+            local status = this.status or this.localstatus
+            local viewheight = this.scrollframe:GetHeight()
+            local height = this.content:GetHeight()
+            local offset
+
+            if viewheight > height then
+                offset = 0
+            else
+                offset = floor((height - viewheight) / 1000.0 * value)
+            end
+            this.content:ClearAllPoints()
+            this.content:SetPoint("TOPLEFT", 0, offset)
+            this.content:SetPoint("TOPRIGHT", 0, offset)
+            status.offset = offset
+            status.scrollvalue = value
+        end,
+
+        ["MoveScroll"] = function(this, value)
+            local status = this.status or this.localstatus
+            local height, viewheight = this.scrollframe:GetHeight(), this.content:GetHeight()
+
+            if this.scrollBarShown then
+                local diff = height - viewheight
+                local delta = 1
+                if value < 0 then delta = -1 end
+                this.scrollbar:SetValue(min(max(status.scrollvalue + delta * (1000 / (diff / 45)), 0), 1000))
+            end
+        end,
+
+        ["FixScroll"] = function(this)
+            if this.updateLock then return end
+            this.updateLock = true
+            local status = this.status or this.localstatus
+            local height, viewheight = this.scrollframe:GetHeight(), this.content:GetHeight()
+            local offset = status.offset or 0
+            -- Give us a margin of error of 2 pixels to stop some conditions that i would blame on floating point inaccuracys
+            -- No-one is going to miss 2 pixels at the bottom of the frame, anyhow!
+            if viewheight < height + 2 then
+                if this.scrollBarShown then
+                    this.scrollBarShown = nil
+                    this.scrollbar:Hide()
+                    this.scrollbar:SetValue(0)
+                    this.scrollframe:SetPoint("BOTTOMRIGHT")
+                    if this.content.original_width then
+                        this.content.width = this.content.original_width
+                    end
+                    this:DoLayout()
+                end
+            else
+                if not this.scrollBarShown then
+                    this.scrollBarShown = true
+                    this.scrollbar:Show()
+                    this.scrollframe:SetPoint("BOTTOMRIGHT", -20, 0)
+                    if this.content.original_width then
+                        this.content.width = this.content.original_width - 20
+                    end
+                    this:DoLayout()
+                end
+                local value = (offset / (viewheight - height) * 1000)
+                if value > 1000 then value = 1000 end
+                this.scrollbar:SetValue(value)
+                this:SetScroll(value)
+                if value < 1000 then
+                    this.content:ClearAllPoints()
+                    this.content:SetPoint("TOPLEFT", 0, offset)
+                    this.content:SetPoint("TOPRIGHT", 0, offset)
+                    status.offset = offset
+                end
+            end
+            this.updateLock = nil
+        end,
+
+        ["LayoutFinished"] = function(this, width, height)
+            this.content:SetHeight((height or 0) + 20)
+
+            -- update the scrollframe
+            this:FixScroll()
+
+            -- schedule another update when everything has "settled"
+            this.scrollframe:SetScript("OnUpdate", FixScrollOnUpdate)
+        end,
+
+        ["UpdateTheme"] = updateTheme,
+
+        ["SetStatusTable"] = function(this, status)
+            assert(type(status) == "table")
+            this.status = status
+            if not status.scrollvalue then status.scrollvalue = 0 end
+        end,
+
+        ["OnWidthSet"] = function(this, width)
+            local content = this.content
+            content.width = width - (this.scrollBarShown and 20 or 0)
+            content.original_width = width
+        end,
+
+        ["OnHeightSet"] = function(this, height)
+            local content = this.content
+            content.height = height
+        end
+    }
+    --[[-----------------------------------------------------------------------------
+    Constructor
+    -------------------------------------------------------------------------------]]
+    local function Constructor()
+        local frame = CreateFrame("Frame", nil, UIParent)
+        local num = AceGUI:GetNextWidgetNum(Type)
+
+        local scrollframe = CreateFrame("ScrollFrame", nil, frame)
+        scrollframe:SetPoint("TOPLEFT")
+        scrollframe:SetPoint("BOTTOMRIGHT")
+        scrollframe:EnableMouseWheel(true)
+        scrollframe:SetScript("OnMouseWheel", ScrollFrame_OnMouseWheel)
+        scrollframe:SetScript("OnSizeChanged", ScrollFrame_OnSizeChanged)
+
+        local scrollbar = CreateFrame("Slider", ("AceConfigDialogScrollFrame%dScrollBar"):format(num), scrollframe,
+                                      "UIPanelScrollBarTemplate")
+        scrollbar:SetPoint("TOPLEFT", scrollframe, "TOPRIGHT", 4, -16)
+        scrollbar:SetPoint("BOTTOMLEFT", scrollframe, "BOTTOMRIGHT", 4, 16)
+        scrollbar:SetMinMaxValues(0, 1000)
+        scrollbar:SetValueStep(1)
+        scrollbar:SetValue(0)
+        scrollbar:SetWidth(16)
+        scrollbar:Hide()
+        -- set the script as the last step, so it doesn't fire yet
+        scrollbar:SetScript("OnValueChanged", ScrollBar_OnScrollValueChanged)
+
+        local scrollbg = scrollbar:CreateTexture(nil, "BACKGROUND")
+        scrollbg:SetAllPoints(scrollbar)
+        scrollbg:SetColorTexture(0, 0, 0, 0.4)
+
+        -- Container Support
+        local content = CreateFrame("Frame", nil, scrollframe)
+        content:SetPoint("TOPLEFT")
+        content:SetPoint("TOPRIGHT")
+        content:SetHeight(400)
+        scrollframe:SetScrollChild(content)
+
+        local widget = {
+            localstatus = {scrollvalue = 0},
+            scrollframe = scrollframe,
+            scrollbar = scrollbar,
+            content = content,
+            frame = frame,
+            type = Type
+        }
+        for method, func in pairs(methods) do widget[method] = func end
+        scrollframe.obj, scrollbar.obj = widget, widget
+
+        return AceGUI:RegisterAsContainer(widget)
+    end
+
+    AceGUI:RegisterWidgetType(Type, Constructor, Version)
+
 end
 
 function addon.ui.v2:RegisterRXPV2CurrentStepFrame()
@@ -92,13 +318,9 @@ function addon.ui.v2:RegisterRXPV2CurrentStepFrame()
             content.width = contentwidth
         end,
 
-        ["GetWidth"] = function(this)
-            return this.content.width
-        end,
+        ["GetWidth"] = function(this) return this.content.width end,
 
-        ["GetHeight"] = function(this)
-            return this.content.height
-        end,
+        ["GetHeight"] = function(this) return this.content.height end,
 
         ["SetSize"] = function(this, width, height)
             this:SetWidth(width)
@@ -126,6 +348,8 @@ function addon.ui.v2:RegisterRXPV2CurrentStepFrame()
             end
         end,
 
+        ["UpdateTheme"] = updateTheme,
+
         ["Hide"] = function(this) this.frame:Hide() end,
 
         ["Show"] = function(this) this.frame:Show() end,
@@ -138,9 +362,7 @@ function addon.ui.v2:RegisterRXPV2CurrentStepFrame()
             end
         end,
 
-        ["EnableResize"] = function(this, state)
-            this.frame:SetResizable(state or false)
-        end,
+        ["EnableResize"] = function(this, state) this.frame:SetResizable(state or false) end,
 
         -- called to set an external table to store status in
         ["SetStatusTable"] = function(this, status)
@@ -235,7 +457,7 @@ function addon.ui.v2:RegisterRXPV2CurrentStepFrame()
             sizer = sizer,
             content = content,
             frame = frame,
-            type = Type,
+            type = Type
         }
         for method, func in pairs(methods) do widget[method] = func end
 
@@ -249,9 +471,6 @@ end
 function addon.ui.v2:RegisterRXPV2CurrentStepItem()
     local Type, Version = "RXPV2CurrentStepItem", 1
     if not AceGUI or (AceGUI:GetWidgetVersion(Type) or 0) >= Version then return end
-
-    -- WoW APIs
-    local CreateFrame, UIParent = CreateFrame, UIParent
 
     --[[-----------------------------------------------------------------------------
     Methods
@@ -285,9 +504,7 @@ function addon.ui.v2:RegisterRXPV2CurrentStepItem()
         ["OnWidthSet"] = function(this, width)
             local content = this.content
             local contentwidth = width - 20
-            if contentwidth < 0 then
-                contentwidth = 0
-            end
+            if contentwidth < 0 then contentwidth = 0 end
             content:SetWidth(contentwidth)
             content.width = contentwidth
         end,
@@ -295,12 +512,12 @@ function addon.ui.v2:RegisterRXPV2CurrentStepItem()
         ["OnHeightSet"] = function(this, height)
             local content = this.content
             local contentheight = height - 20
-            if contentheight < 0 then
-                contentheight = 0
-            end
+            if contentheight < 0 then contentheight = 0 end
             content:SetHeight(contentheight)
             content.height = contentheight
-        end
+        end,
+
+        ["UpdateTheme"] = updateTheme,
     }
 
     --[[-----------------------------------------------------------------------------
@@ -345,22 +562,14 @@ function addon.ui.v2:RegisterRXPV2CurrentStepItem()
         -- border:SetBackdropColor(unpack(addon.colors.bottomFrameBG))
         -- border:SetBackdropBorderColor(unpack(addon.colors.bottomFrameBG))
 
-        --Container Support
+        -- Container Support
         local content = CreateFrame("Frame", nil, frame)
         content:SetPoint("TOPLEFT", 6, -10)
         content:SetPoint("BOTTOMRIGHT", -6, 6)
 
-        local widget = {
-            title     = title,
-            titletext = titletext,
-            content   = content,
-            frame     = frame,
-            type      = Type,
-        }
+        local widget = {title = title, titletext = titletext, content = content, frame = frame, type = Type}
 
-        for method, func in pairs(methods) do
-            widget[method] = func
-        end
+        for method, func in pairs(methods) do widget[method] = func end
 
         return AceGUI:RegisterAsContainer(widget)
     end
